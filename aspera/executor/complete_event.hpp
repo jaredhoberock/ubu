2dfebd5469f0a58e2a44310c9cebb57b502f1d92
Jared Hoberock <jaredhoberock@gmail.com>
2021-10-25 16:20:51 -0500

    Introduce and test complete_event

---
+++ aspera/executor/complete_event.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../detail/prologue.hpp"
+
+
+ASPERA_NAMESPACE_OPEN_BRACE
+
+
+struct complete_event
+{
+  void wait() {}
+};
+
+
+ASPERA_NAMESPACE_CLOSE_BRACE
+
+
+#include "../detail/epilogue.hpp"
+
---
+++ tests/executor/complete_event.hpp
@@ -0,0 +1,39 @@
+#include <aspera/executor/complete_event.hpp>
+#include <aspera/executor/wait.hpp>
+#include <cassert>
+
+#ifdef __CUDACC__
+#include <cuda_runtime_api.h>
+
+template<class F>
+__global__ void device_invoke(F f)
+{
+  f();
+}
+#endif
+
+namespace ns = aspera;
+
+
+void test()
+{
+  {
+    ns::complete_event e;
+    ns::wait(e);
+  }
+}
+
+
+void test_complete_event()
+{
+  test();
+
+#ifdef __CUDACC__
+  device_invoke<<<1,1>>>([] ()
+  {
+    test();
+  });
+  assert(cudaDeviceSynchronize() == cudaSuccess);
+#endif
+}
+
