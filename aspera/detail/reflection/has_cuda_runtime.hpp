206b04287489d8c8126e371e409c11154635a803
Jared Hoberock <jaredhoberock@gmail.com>
2021-10-31 23:21:44 -0500

    Introduce has_cuda_runtime.hpp

---
+++ aspera/detail/reflection/has_cuda_runtime.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../prologue.hpp"
+
+#include "is_host.hpp"
+
+constexpr bool has_cuda_runtime()
+{
+  bool result = false;
+
+
+#if defined(__circle_lang__)
+  if target(is_host())
+  {
+    // XXX we could consider using #__has_include(<cuda_runtime_api.h>) here
+    result = true;
+  }
+  else if target(__nvvm_arch >= nvvm_arch_t{35})
+  {
+#  if defined(__CUDACC_RDC__)
+    // in __device__ code, using the CUDA Runtime requires relocatable device code
+    result = true;
+#  endif // __CUDACC_RDC__
+  }
+#else // __circle_lang__
+  result = true;
+#endif
+
+  return result;
+}
+
+#include "../epilogue.hpp"
+
