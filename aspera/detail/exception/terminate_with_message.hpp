72eed33b7af64b150d0e7eb237ceb3759b214402
Jared Hoberock <jaredhoberock@gmail.com>
2021-10-31 23:23:23 -0500

    Introduce terminate_with_message.hpp

---
+++ aspera/detail/exception/terminate_with_message.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../prologue.hpp"
+
+#include "terminate.hpp"
+
+
+ASPERA_NAMESPACE_OPEN_BRACE
+
+
+namespace detail
+{
+
+
+inline void terminate_with_message(const char* message) noexcept
+{
+  printf("%s\n", message);
+
+  terminate();
+}
+
+
+} // end detail
+
+
+ASPERA_NAMESPACE_CLOSE_BRACE
+
+
+#include "../epilogue.hpp"
+
