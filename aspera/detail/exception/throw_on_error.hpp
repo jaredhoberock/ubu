4b02a7a6c8561c4e4be015fd68e90dea1f76a708
Jared Hoberock <jaredhoberock@gmail.com>
2021-10-31 23:23:50 -0500

    Introduce throw_on_error.hpp

---
+++ aspera/detail/exception/throw_on_error.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "../prologue.hpp"
+
+#include "../reflection.hpp"
+#include "terminate.hpp"
+#include <cuda_runtime_api.h>
+#include <stdexcept>
+
+
+ASPERA_NAMESPACE_OPEN_BRACE
+
+
+namespace detail
+{
+
+
+inline void throw_on_error(cudaError_t e, const char* message)
+{
+  if(e)
+  {
+    if ASPERA_TARGET(is_host())
+    {
+      std::string what = std::string(message) + std::string(": ") + cudaGetErrorString(e);
+      throw std::runtime_error(what);
+    }
+    else
+    {
+      if ASPERA_TARGET(has_cuda_runtime())
+      {
+        message = cudaGetErrorString(e);
+
+        printf("Error after %s: %s\n", message, cudaGetErrorString(e));
+      }
+      else
+      {
+        printf("Error: %s\n", message);
+      }
+
+      terminate();
+    }
+  }
+}
+
+
+} // end detail
+
+
+ASPERA_NAMESPACE_CLOSE_BRACE
+
+#include "../epilogue.hpp"
+
