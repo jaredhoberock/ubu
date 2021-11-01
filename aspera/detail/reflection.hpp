19ddacd7a6259d3f336b44d25579eabbe3751ff4
Jared Hoberock <jaredhoberock@gmail.com>
2021-10-31 23:22:47 -0500

    Introduce reflection.hpp

---
+++ aspera/detail/reflection.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "prologue.hpp"
+
+#include "reflection/has_cuda_runtime.hpp"
+#include "reflection/is_device.hpp"
+#include "reflection/is_host.hpp"
+
+#include "epilogue.hpp"
+
