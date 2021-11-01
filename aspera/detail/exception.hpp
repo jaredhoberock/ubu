0ff0d1ebf80f4b00bf40ec85c64359d772dd51ba
Jared Hoberock <jaredhoberock@gmail.com>
2021-10-31 23:24:54 -0500

    Introduce exception.hpp

---
+++ aspera/detail/exception.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "prologue.hpp"
+
+#include "exception/terminate.hpp"
+#include "exception/terminate_with_message.hpp"
+#include "exception/throw_on_error.hpp"
+#include "exception/throw_runtime_error.hpp"
+
+#include "epilogue.hpp"
+
