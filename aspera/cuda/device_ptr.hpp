7134d331f8db6bfdfaf4e60207ffe2ea132b250d
Jared Hoberock <jaredhoberock@gmail.com>
2022-01-29 20:09:22 -0600

    Introduce and test copy_n_after

--- aspera/cuda/device_ptr.hpp
+++ aspera/cuda/device_ptr.hpp
@@ -5,6 +5,9 @@
 #include "../detail/exception.hpp"
 #include "../memory/pointer_adaptor.hpp"
 #include "detail/temporarily_with_current_device.hpp"
+#include "event.hpp"
+#include "kernel_executor.hpp"
+#include <concepts>
 #include <type_traits>
 
 
@@ -86,15 +89,56 @@ std::remove_cv_t<T>* copy_n(device_ptr<T> first, std::size_t count, std::remove_
   return first.copier().copy_n_to_raw_pointer(first.native_handle(), count, result);
 }
 
-template<class T>
-  requires std::is_assignable_v<T,T>
-device_ptr<std::remove_cv_t<T>> copy_n(device_ptr<T> first, std::size_t count, device_ptr<std::remove_cv_t<T>> result)
+template<class T, class U>
+  requires std::is_assignable_v<T,U>
+device_ptr<T> copy_n(device_ptr<U> first, std::size_t count, device_ptr<T> result)
 {
   return first.copier().copy_n(first.native_handle(), count, result.native_handle());
 }
 
 
+template<class T, class U>
+  requires (std::is_trivially_assignable_v<T&,U> and std::same_as<U,std::remove_cv_t<T>>)
+event copy_n_after(kernel_executor ex, event&& before, device_ptr<T> from, std::size_t count, device_ptr<U> to)
+{
+  // make the stream wait for the before event
+  detail::throw_on_error(cudaStreamWaitEvent(ex.stream(), before.native_handle()),
+    "cuda::copy_n_after: CUDA error after cudaStreamWaitEvent"
+  );
+
+  // enqueue a cudaMemcpyAsync
+  detail::throw_on_error(cudaMemcpyAsync(to.native_handle(), from.native_handle(), sizeof(U) * count, cudaMemcpyDeviceToDevice, ex.stream()),
+    "cuda::copy_n_after: CUDA error after cudaMemcpyAsync"
+  );
+
+  // reuse the input event
+  before.record_on(ex.stream());
+  
+  return std::move(before);
+}
+
+
+template<class T, class U>
+  requires (std::is_trivially_assignable_v<T&,U> and std::same_as<U,std::remove_cv_t<T>>)
+event copy_n_after(kernel_executor ex, const event& before, device_ptr<T> from, std::size_t count, device_ptr<U> to)
+{
+  // make the stream wait for the before event
+  detail::throw_on_error(cudaStreamWaitEvent(ex.stream(), before.native_handle()),
+    "cuda::copy_n_after: CUDA error after cudaStreamWaitEvent"
+  );
+
+  // enqueue a cudaMemcpyAsync
+  detail::throw_on_error(cudaMemcpyAsync(to.native_handle(), from.native_handle(), sizeof(U) * count, cudaMemcpyDeviceToDevice, ex.stream()),
+    "cuda::copy_n_after: CUDA error after cudaMemcpyAsync"
+  );
+
+  // create a new event
+  return event{ex.device(), ex.stream()};
+}
+
+
 } // end cuda
+
 
 ASPERA_NAMESPACE_CLOSE_BRACE
 
--- tests/cuda/device_ptr.hpp
+++ tests/cuda/device_ptr.hpp
@@ -1,18 +1,132 @@
+#include <aspera/cuda/device_memory_resource.hpp>
 #include <aspera/cuda/device_ptr.hpp>
+#include <aspera/cuda/kernel_executor.hpp>
 
 #undef NDEBUG
 #include <cassert>
 
+#include <cuda_runtime_api.h>
+
 namespace ns = aspera;
 
 
-void test_device_ptr()
+void test_copy_n_after()
 {
-  using namespace ns;
+  using namespace ns::cuda;
+
+  device_memory_resource mem0{0}, mem1{1};
+
+  // allocate an int on each of two devices
+  device_ptr<int> d_ptr0{reinterpret_cast<int*>(mem0.allocate(sizeof(int))), 0};
+  device_ptr<int> d_ptr1{reinterpret_cast<int*>(mem1.allocate(sizeof(int))), 1};
+
+  *d_ptr0 = 7;
+  *d_ptr1 = 13;
+
+  assert(7 == *d_ptr0);
+  assert(13 == *d_ptr1);
+
+  cudaStream_t s0 = aspera::detail::temporarily_with_current_device(0, [&]
+  {
+    cudaStream_t result{};
+    cudaStreamCreate(&result);
+    return result;
+  });
+
+  cudaStream_t s1 = aspera::detail::temporarily_with_current_device(1, [&]
+  {
+    cudaStream_t result{};
+    cudaStreamCreate(&result);
+    return result;
+  });
+
+
+  {
+    // test copy from device 0 to device 1 on device 0
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    kernel_executor ex0{s0,0};
+
+    event before{ex0.device()};
+    auto after = copy_n_after(ex0, before, d_ptr0, 1, d_ptr1);
+    after.wait();
+
+    assert(7 == *d_ptr0);
+    assert(7 == *d_ptr1);
+    assert(*d_ptr0 == *d_ptr1);
+
+    *d_ptr1 = 13;
+  }
+
+  {
+    // test copy from device 0 to device 1 on device 1
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    kernel_executor ex1{s1,1};
+
+    event before{ex1.device()};
+    auto after = copy_n_after(ex1, before, d_ptr0, 1, d_ptr1);
+    after.wait();
+
+    assert(7 == *d_ptr0);
+    assert(7 == *d_ptr1);
+    assert(*d_ptr0 == *d_ptr1);
+
+    *d_ptr1 = 13;
+  }
+
+  {
+    // test copy from device 1 to device 0 on device 0
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    kernel_executor ex0{s0,0};
+
+    event before{ex0.device()};
+    auto after = copy_n_after(ex0, before, d_ptr1, 1, d_ptr0);
+    after.wait();
+
+    assert(13 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    *d_ptr0 = 7;
+  }
+
+  {
+    // test copy from device 1 to device 0 on device 1
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    kernel_executor ex1{s1,1};
+
+    event before{ex1.device()};
+    auto after = copy_n_after(ex1, before, d_ptr1, 1, d_ptr0);
+    after.wait();
+
+    assert(13 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    *d_ptr0 = 7;
+  }
+
+
+  mem0.deallocate(d_ptr0.native_handle(), sizeof(int));
+  mem1.deallocate(d_ptr1.native_handle(), sizeof(int));
+
+  cudaStreamDestroy(s0);
+  cudaStreamDestroy(s1);
+}
+
+
+void test_constructors()
+{
+  using namespace ns::cuda;
 
   {
     // test default construction
-    cuda::device_ptr<int> ptr;
+    device_ptr<int> ptr;
 
     // silence "declared but never referenced" warnings
     static_cast<void>(ptr);
@@ -20,88 +134,217 @@ void test_device_ptr()
 
   {
     // test construction from nullptr
-    cuda::device_ptr<int> ptr{nullptr};
+    device_ptr<int> ptr{nullptr};
 
     assert(ptr.native_handle() == nullptr);
     assert(!ptr);
   }
+}
+
+
+void test_writeable_device_ptr()
+{
+  using namespace ns::cuda;
+
+  int* d_array{};
+  assert(cudaMalloc(reinterpret_cast<void**>(&d_array), 4 * sizeof(int)) == cudaSuccess);
+  int h_array[] = {0, 1, 2, 3};
+  
+  assert(cudaMemcpy(d_array, h_array, 4 * sizeof(int), cudaMemcpyDefault) == cudaSuccess);
+  
+  // test construction from raw pointer
+  device_ptr<int> ptr{d_array};
+  
+  // test native_handle
+  for(int i = 0; i < 4; ++i)
+  {
+    assert((ptr + i).native_handle() == d_array + i);
+  }
+  
+  // test dereference
+  for(int i = 0; i < 4; ++i)
+  {
+    assert(*(ptr + i) == h_array[i]);
+  }
+  
+  // test subscript
+  for(int i = 0; i < 4; ++i)
+  {
+    assert(ptr[i] == h_array[i]);
+  }
+  
+  // test store
+  for(int i = 0; i < 4; ++i)
+  {
+    ptr[i] = 4 - i;
+  }
+  
+  assert(cudaMemcpy(h_array, d_array, 4 * sizeof(int), cudaMemcpyDefault) == cudaSuccess);
+  for(int i = 0; i < 4; ++i)
+  {
+    assert(h_array[i] == 4 - i);
+  }
+  
+  assert(cudaFree(d_array) == cudaSuccess);
+}
+
+
+void test_readable_device_ptr()
+{
+  using namespace ns::cuda;
+
+  int* d_array{};
+  assert(cudaMalloc(reinterpret_cast<void**>(&d_array), 4 * sizeof(int)) == cudaSuccess);
+  int h_array[] = {0, 1, 2, 3};
+  
+  assert(cudaMemcpy(d_array, h_array, 4 * sizeof(int), cudaMemcpyDefault) == cudaSuccess);
+  
+  // test construction from raw pointer
+  device_ptr<const int> ptr{d_array};
+  
+  // test native_handle
+  for(int i = 0; i < 4; ++i)
+  {
+    assert((ptr + i).native_handle() == d_array + i);
+  }
+  
+  // test dereference
+  for(int i = 0; i < 4; ++i)
+  {
+    assert(*(ptr + i) == h_array[i]);
+  }
+  
+  // test subscript
+  for(int i = 0; i < 4; ++i)
+  {
+    assert(ptr[i] == h_array[i]);
+  }
+  
+  assert(cudaFree(d_array) == cudaSuccess);
+}
+
+
+void test_copy_n_after_between_devices()
+{
+  using namespace ns::cuda;
+
+  device_memory_resource mem0{0}, mem1{1};
+
+  // allocate an int on each of two devices
+  device_ptr<int> d_ptr0{reinterpret_cast<int*>(mem0.allocate(sizeof(int))), 0};
+  device_ptr<int> d_ptr1{reinterpret_cast<int*>(mem1.allocate(sizeof(int))), 1};
+
+  *d_ptr0 = 7;
+  *d_ptr1 = 13;
+
+  assert(7 == *d_ptr0);
+  assert(13 == *d_ptr1);
+
+  cudaStream_t s0 = aspera::detail::temporarily_with_current_device(0, [&]
+  {
+    cudaStream_t result{};
+    cudaStreamCreate(&result);
+    return result;
+  });
+
+  cudaStream_t s1 = aspera::detail::temporarily_with_current_device(1, [&]
+  {
+    cudaStream_t result{};
+    cudaStreamCreate(&result);
+    return result;
+  });
+
 
   {
-    // test writable device_ptr
+    // test copy from device 0 to device 1 on device 0
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
 
-    int* d_array{};
-    assert(cudaMalloc(reinterpret_cast<void**>(&d_array), 4 * sizeof(int)) == cudaSuccess);
-    int h_array[] = {0, 1, 2, 3};
+    kernel_executor ex0{s0,0};
 
-    assert(cudaMemcpy(d_array, h_array, 4 * sizeof(int), cudaMemcpyDefault) == cudaSuccess);
+    event before{ex0.device()};
+    auto after = copy_n_after(ex0, before, d_ptr0, 1, d_ptr1);
+    after.wait();
 
-    // test construction from raw pointer
-    cuda::device_ptr<int> ptr{d_array};
+    assert(7 == *d_ptr0);
+    assert(7 == *d_ptr1);
+    assert(*d_ptr0 == *d_ptr1);
 
-    // test native_handle
-    for(int i = 0; i < 4; ++i)
-    {
-      assert((ptr + i).native_handle() == d_array + i);
-    }
-
-    // test dereference
-    for(int i = 0; i < 4; ++i)
-    {
-      assert(*(ptr + i) == h_array[i]);
-    }
-
-    // test subscript
-    for(int i = 0; i < 4; ++i)
-    {
-      assert(ptr[i] == h_array[i]);
-    }
-
-    // test store
-    for(int i = 0; i < 4; ++i)
-    {
-      ptr[i] = 4 - i;
-    }
-
-    assert(cudaMemcpy(h_array, d_array, 4 * sizeof(int), cudaMemcpyDefault) == cudaSuccess);
-    for(int i = 0; i < 4; ++i)
-    {
-      assert(h_array[i] == 4 - i);
-    }
-
-    assert(cudaFree(d_array) == cudaSuccess);
+    *d_ptr1 = 13;
   }
 
   {
-    // test readable device_ptr
+    // test copy from device 0 to device 1 on device 1
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
 
-    int* d_array{};
-    assert(cudaMalloc(reinterpret_cast<void**>(&d_array), 4 * sizeof(int)) == cudaSuccess);
-    int h_array[] = {0, 1, 2, 3};
+    kernel_executor ex1{s1,1};
 
-    assert(cudaMemcpy(d_array, h_array, 4 * sizeof(int), cudaMemcpyDefault) == cudaSuccess);
+    event before{ex1.device()};
+    auto after = copy_n_after(ex1, before, d_ptr0, 1, d_ptr1);
+    after.wait();
 
-    // test construction from raw pointer
-    cuda::device_ptr<const int> ptr{d_array};
+    assert(7 == *d_ptr0);
+    assert(7 == *d_ptr1);
+    assert(*d_ptr0 == *d_ptr1);
 
-    // test native_handle
-    for(int i = 0; i < 4; ++i)
-    {
-      assert((ptr + i).native_handle() == d_array + i);
-    }
+    *d_ptr1 = 13;
+  }
 
-    // test dereference
-    for(int i = 0; i < 4; ++i)
-    {
-      assert(*(ptr + i) == h_array[i]);
-    }
+  {
+    // test copy from device 1 to device 0 on device 0
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
 
-    // test subscript
-    for(int i = 0; i < 4; ++i)
-    {
-      assert(ptr[i] == h_array[i]);
-    }
+    kernel_executor ex0{s0,0};
 
-    assert(cudaFree(d_array) == cudaSuccess);
+    event before{ex0.device()};
+    auto after = copy_n_after(ex0, before, d_ptr1, 1, d_ptr0);
+    after.wait();
+
+    assert(13 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    *d_ptr0 = 7;
+  }
+
+  {
+    // test copy from device 1 to device 0 on device 1
+    assert(7 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    kernel_executor ex1{s1,1};
+
+    event before{ex1.device()};
+    auto after = copy_n_after(ex1, before, d_ptr1, 1, d_ptr0);
+    after.wait();
+
+    assert(13 == *d_ptr0);
+    assert(13 == *d_ptr1);
+
+    *d_ptr0 = 7;
+  }
+
+  mem0.deallocate(d_ptr0.native_handle(), sizeof(int));
+  mem1.deallocate(d_ptr1.native_handle(), sizeof(int));
+
+  assert(cudaStreamDestroy(s0) == cudaSuccess);
+  assert(cudaStreamDestroy(s1) == cudaSuccess);
+}
+
+
+void test_device_ptr()
+{
+  test_constructors();
+  test_writeable_device_ptr();
+  test_readable_device_ptr();
+
+  int num_devices{};
+  assert(cudaGetDeviceCount(&num_devices) == cudaSuccess);
+
+  if(num_devices > 1)
+  {
+    test_copy_n_after_between_devices();
   }
 }
 
