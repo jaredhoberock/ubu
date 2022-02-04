00689b83362629357b448d4cb1a81d45d8e961cf
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-04 17:44:33 -0600

    Customize contingent_on for complete_event

--- aspera/event/complete_event.hpp
+++ aspera/event/complete_event.hpp
@@ -2,6 +2,8 @@
 
 #include "../detail/prologue.hpp"
 
+#include <concepts>
+
 
 ASPERA_NAMESPACE_OPEN_BRACE
 
@@ -11,6 +13,15 @@ struct complete_event
   constexpr void wait() {}
 
   constexpr static complete_event make_complete_event()
+  {
+    return {};
+  }
+
+  // for any given executor, and a set of complete_events, we can always
+  // create a complete_event contingent on all of them
+  // this is a friend function to avoid ambiguity with the CPO aspera::contingent_on
+  template<class E>
+  constexpr friend complete_event contingent_on(E&&, const complete_event&...)
   {
     return {};
   }
