352269ec27afe01c723675901857e09b0d3c0e78
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-04 15:23:46 -0600

    Update umbrella header

--- aspera/memory/allocator.hpp
+++ aspera/memory/allocator.hpp
@@ -2,15 +2,22 @@
 
 #include "../detail/prologue.hpp"
 
+#include "allocator/allocate.hpp"
 #include "allocator/allocate_after.hpp"
+#include "allocator/allocator.hpp"
+#include "allocator/allocator_value_t.hpp"
 #include "allocator/asynchronous_allocator.hpp"
 #include "allocator/asynchronous_deallocator.hpp"
+#include "allocator/construct.hpp"
+#include "allocator/deallocate.hpp"
 #include "allocator/deallocate_after.hpp"
 #include "allocator/delete_after.hpp"
 #include "allocator/destroy.hpp"
 #include "allocator/destroy_after.hpp"
 #include "allocator/finally_deallocate_after.hpp"
 #include "allocator/finally_delete_after.hpp"
+#include "allocator/first_allocate.hpp"
+#include "allocator/rebind_allocator.hpp"
 
 #include "../detail/epilogue.hpp"
 
