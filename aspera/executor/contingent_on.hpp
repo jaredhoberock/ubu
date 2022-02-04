cc143b4b7ea4a8f56ce29977268140504bc25cd3
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-04 17:45:42 -0600

    When possible, avoid returning an event_tuple from contingent_on

--- aspera/executor/contingent_on.hpp
+++ aspera/executor/contingent_on.hpp
@@ -4,6 +4,7 @@
 
 #include "../event/event.hpp"
 #include "../event/wait.hpp"
+#include "executor.hpp"
 #include <type_traits>
 #include <tuple>
 #include <utility>
@@ -78,15 +79,25 @@ struct dispatch_contingent_on
     return contingent_on(std::forward<Ex>(executor), std::forward<Events>(events)...);
   }
 
-  // the default path moves the events into an event_tuple
-  template<class Ex, class... Events>
-    requires (!has_contingent_on_member_function<Ex&&,Events&&...> and
-              !has_contingent_on_free_function<Ex&&,Events&&...> and
+  // the default path for a single event simply moves the event
+  template<executor Ex, event E>
+    requires (!has_contingent_on_member_function<Ex&&,E&&> and
+              !has_contingent_on_free_function<Ex&&,E&&> and
+              std::is_move_constructible_v<std::remove_reference_t<E>>)
+  constexpr std::remove_cvref_t<E> operator()(Ex&&, E&& event) const
+  {
+    return std::move(event);
+  }
+
+  // the default path for many events moves the events into an event_tuple
+  template<executor Ex, event... Es>
+    requires (!has_contingent_on_member_function<Ex&&,Es&&...> and
+              !has_contingent_on_free_function<Ex&&,Es&&...> and
               std::conjunction_v<
-                std::is_move_constructible<std::remove_reference_t<Events>>...
+                std::is_move_constructible<std::remove_reference_t<Es>>...
               >)
-  constexpr event_tuple<std::remove_cvref_t<Events>...>
-    operator()(Ex&&, Events&&... events) const
+  constexpr event_tuple<std::remove_cvref_t<Es>...>
+    operator()(Ex&&, Es&&... events) const
   {
     return {std::move(events)...};
   }
