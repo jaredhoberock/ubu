92dc59243c2f85020843619df03f055c2647587c
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-04 17:49:20 -0600

    Update executor umbrella header

--- aspera/executor.hpp
+++ aspera/executor.hpp
@@ -2,12 +2,17 @@
 
 #include "detail/prologue.hpp"
 
+#include "executor/associated_executor.hpp"
+#include "executor/contingent_on.hpp"
 #include "executor/contingent_on_all.hpp"
 #include "executor/execute.hpp"
+#include "executor/execute_after.hpp"
 #include "executor/executor.hpp"
+#include "executor/executor_associate.hpp"
 #include "executor/executor_event.hpp"
 #include "executor/executor_of.hpp"
 #include "executor/first_execute.hpp"
+#include "executor/inline_executor.hpp"
 #include "executor/new_thread_executor.hpp"
 
 #include "detail/epilogue.hpp"
