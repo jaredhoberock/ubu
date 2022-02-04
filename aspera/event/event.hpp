5053170c6c2bea50a7941bcf0c1e789b5a1300e9
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-04 17:50:43 -0600

    Require nothrow move constructible and destructible from event

--- aspera/event/event.hpp
+++ aspera/event/event.hpp
@@ -9,11 +9,15 @@ ASPERA_NAMESPACE_OPEN_BRACE
 
 
 template<class E>
-concept event = requires(std::remove_cvref_t<E> e)
-{
-  // a lvalue ref to e must be able to wait
-  ASPERA_NAMESPACE::wait(e);
-};
+concept event = 
+  std::is_nothrow_move_constructible_v<E> and
+  std::is_nothrow_destructible_v<E> and
+  requires(std::remove_cvref_t<E> e)
+  {
+    // a lvalue ref to e must be able to wait
+    ASPERA_NAMESPACE::wait(e);
+  }
+;
 
 
 ASPERA_NAMESPACE_CLOSE_BRACE
