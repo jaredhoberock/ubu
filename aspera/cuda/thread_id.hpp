7f9b01aae59e24939713507a1e2330395f8aa119
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-24 15:28:07 -0600

    Eliminate CUDA vector types in thread_id

--- aspera/cuda/thread_id.hpp
+++ aspera/cuda/thread_id.hpp
@@ -2,75 +2,34 @@
 
 #include "../detail/prologue.hpp"
 
+#include "../coordinate/grid_coordinate.hpp"
+#include "../coordinate/point.hpp"
+#include <vector_types.h>
+
 
 ASPERA_NAMESPACE_OPEN_BRACE
-
-#include <vector_types.h>
 
 
 namespace cuda
 {
 
 
-constexpr std::size_t size(const ::int3&)
-{
-  return 3;
-}
-
-template<std::size_t i>
-constexpr const int& element(const ::int3&);
-
-template<std::size_t i>
-constexpr int& element(::int3&);
-
-template<>
-constexpr const int& element<0>(const ::int3& d)
-{
-  return d.x;
-}
-
-template<>
-constexpr int& element<0>(::int3& d)
-{
-  return d.x;
-}
-
-template<>
-constexpr const int& element<1>(const ::int3& d)
-{
-  return d.y;
-}
-
-template<>
-constexpr int& element<1>(::int3& d)
-{
-  return d.y;
-}
-
-template<>
-constexpr const int& element<2>(const ::int3& d)
-{
-  return d.z;
-}
-
-template<>
-constexpr int& element<2>(::int3& d)
-{
-  return d.z;
-}
-
-
 struct thread_id
 {
-  ::int3 block;
-  ::int3 thread;
+  int3 block;
+  int3 thread;
 
   thread_id() = default;
 
   thread_id(const thread_id&) = default;
 
-  thread_id(::int3 b, ::int3 t)
+  thread_id(int3 b, int3 t)
     : block{b}, thread{t}
+  {}
+
+  thread_id(int2 block_and_thread)
+    : block{block_and_thread.x, 0, 0},
+      thread{block_and_thread.y, 0, 0}
   {}
 
   constexpr static std::size_t size()
@@ -80,31 +39,31 @@ struct thread_id
 };
 
 template<std::size_t i>
-constexpr const ::int3& element(const thread_id&);
+constexpr const int3& element(const thread_id&);
 
 template<std::size_t i>
-constexpr ::int3& element(thread_id&);
+constexpr int3& element(thread_id&);
 
 template<>
-constexpr const ::int3& element<0>(const thread_id& t)
+constexpr const int3& element<0>(const thread_id& t)
 {
   return t.block;
 }
 
 template<>
-constexpr ::int3& element<0>(thread_id& t)
+constexpr int3& element<0>(thread_id& t)
 {
   return t.block;
 }
 
 template<>
-constexpr const ::int3& element<1>(const thread_id& t)
+constexpr const int3& element<1>(const thread_id& t)
 {
   return t.thread;
 }
 
 template<>
-constexpr ::int3& element<1>(thread_id& t)
+constexpr int3& element<1>(thread_id& t)
 {
   return t.thread;
 }
