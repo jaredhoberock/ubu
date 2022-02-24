665ce300afacf0bfda0a01d2b564e29e57817c66
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-24 15:26:06 -0600

    Enable lvalue refs to work with executor_of

--- aspera/execution/executor/executor_of.hpp
+++ aspera/execution/executor/executor_of.hpp
@@ -2,8 +2,9 @@
 
 #include "../../detail/prologue.hpp"
 
-#include <concepts>
 #include "execute.hpp"
+#include <concepts>
+#include <type_traits>
 
 
 ASPERA_NAMESPACE_OPEN_BRACE
@@ -11,8 +12,8 @@ ASPERA_NAMESPACE_OPEN_BRACE
 template<class E, class F>
 concept executor_of =
   std::invocable<F> and
-  std::is_nothrow_copy_constructible_v<E> and
-  std::is_nothrow_destructible_v<E> and
+  std::is_nothrow_copy_constructible_v<std::remove_cvref_t<E>> and
+  std::is_nothrow_destructible_v<std::remove_cvref_t<E>> and
   std::equality_comparable<E> and
   requires(E e, F f){ ASPERA_NAMESPACE::execute(e, f); }
 ;
