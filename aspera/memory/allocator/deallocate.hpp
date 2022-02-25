492f6168933de787441eb765c031e1838f749664
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-24 18:10:27 -0600

    Rebind by default in deallocate

--- aspera/memory/allocator/deallocate.hpp
+++ aspera/memory/allocator/deallocate.hpp
@@ -2,7 +2,9 @@
 
 #include "../../detail/prologue.hpp"
 
+#include "rebind_allocator.hpp"
 #include <memory>
+#include <type_traits>
 #include <utility>
 
 ASPERA_NAMESPACE_OPEN_BRACE
@@ -12,15 +14,32 @@ namespace detail
 {
 
 
+template<class A, class P, class N>
+concept has_allocator_traits_deallocate = requires(A alloc, P p, N n)
+{
+  std::allocator_traits<std::decay_t<A>>::deallocate(alloc, p, n);
+};
+
+
 // this is the type of deallocate
 struct dispatch_deallocate
 {
   // dispatch just uses std::allocator_traits
   template<class Alloc, class P, class N>
-  constexpr auto operator()(Alloc&& alloc, P&& p, N&& n) const
-    -> decltype(std::allocator_traits<std::decay_t<Alloc>>::deallocate(std::forward<Alloc>(alloc), std::forward<P>(p), std::forward<N>(n)))
+    requires has_allocator_traits_deallocate<Alloc&&,P&&,N&&>
+  constexpr void operator()(Alloc&& alloc, P&& p, N&& n) const
   {
-    return std::allocator_traits<std::decay_t<Alloc>>::deallocate(std::forward<Alloc>(alloc), std::forward<P>(p), std::forward<N>(n));
+    std::allocator_traits<std::decay_t<Alloc>>::deallocate(std::forward<Alloc>(alloc), std::forward<P>(p), std::forward<N>(n));
+  }
+
+  // this path attempts to first rebind_allocator and then recurse
+  template<class Alloc, class P, class N>
+    requires (!has_allocator_traits_deallocate<Alloc&&,P&&,N&&> and
+              has_rebind_allocator<typename std::pointer_traits<std::remove_cvref_t<P>>::element_type,Alloc&&>)
+  constexpr decltype(auto) operator()(Alloc&& alloc, P&& p, N&& n) const
+  {
+    auto rebound_alloc = rebind_allocator<typename std::pointer_traits<P>::element_type>(std::forward<Alloc>(alloc));
+    return (*this)(rebound_alloc, std::forward<P>(p), std::forward<N>(n));
   }
 };
 
