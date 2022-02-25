237d7339ec624b125b13a7c8d105dfb54b341189
Jared Hoberock <jaredhoberock@gmail.com>
2022-02-24 18:27:39 -0600

    Rebind by default in finally_deallocate_after

--- aspera/memory/allocator/finally_deallocate_after.hpp
+++ aspera/memory/allocator/finally_deallocate_after.hpp
@@ -3,8 +3,9 @@
 #include "../../detail/prologue.hpp"
 
 #include "../../event/event.hpp"
-#include "asynchronous_deallocator.hpp"
+#include "rebind_allocator.hpp"
 #include <memory>
+#include <type_traits>
 #include <utility>
 
 ASPERA_NAMESPACE_OPEN_BRACE
@@ -27,6 +28,10 @@ concept has_finally_deallocate_after_free_function = requires(A alloc, E before,
 };
 
 
+template<class A, class E, class P, class N>
+concept has_finally_deallocate_after_customization = has_finally_deallocate_after_member_function<A,E,P,N> or has_finally_deallocate_after_free_function<A,E,P,N>;
+
+
 // this is the type of finally_deallocate_after
 struct dispatch_finally_deallocate_after
 {
@@ -47,13 +52,31 @@ struct dispatch_finally_deallocate_after
     return finally_deallocate_after(std::forward<Alloc>(alloc), std::forward<Event>(before), std::forward<P>(ptr), std::forward<N>(n));
   }
 
-  // the default path calls deallocate_after
-  template<asynchronous_deallocator Alloc, class Event, class P, class N>
+  // this dispatch path tries to rebind and then call finally_deallocate_after again
+  template<class Alloc, class Event, class P, class N>
     requires (!has_finally_deallocate_after_member_function<Alloc&&, Event&&, P&&, N&&> and
-              !has_finally_deallocate_after_free_function<Alloc&&, Event&&, P&&, N&&>)
+              !has_finally_deallocate_after_free_function<Alloc&&, Event&&, P&&, N&&> and
+              has_finally_deallocate_after_customization<
+                rebind_allocator_result_t<typename std::pointer_traits<std::remove_cvref_t<P>>::element_type,Alloc&&>,
+                Event&&, P&&, N&&
+              >)
   constexpr void operator()(Alloc&& alloc, Event&& before, P&& ptr, N&& n) const
   {
-    deallocate_after(std::forward<Alloc>(alloc), std::forward<Event>(before), std::forward<P>(ptr), std::forward<N>(n));
+    auto rebound_alloc = rebind_allocator<typename std::pointer_traits<std::remove_cvref_t<P>>::element_type>(std::forward<Alloc>(alloc));
+    return (*this)(rebound_alloc, std::forward<Event>(before), std::forward<P>(ptr), std::forward<N>(n));
+  }
+
+  // the default path calls deallocate_after
+  template<class Alloc, class Event, class P, class N>
+    requires (!has_finally_deallocate_after_member_function<Alloc&&, Event&&, P&&, N&&> and
+              !has_finally_deallocate_after_free_function<Alloc&&, Event&&, P&&, N&&> and
+              !has_finally_deallocate_after_customization<
+                rebind_allocator_result_t<typename std::pointer_traits<std::remove_cvref_t<P>>::element_type,Alloc&&>,
+                Event&&, P&&, N&&
+              >)
+  constexpr decltype(auto) operator()(Alloc&& alloc, Event&& before, P&& ptr, N&& n) const
+  {
+    return deallocate_after(std::forward<Alloc>(alloc), std::forward<Event>(before), std::forward<P>(ptr), std::forward<N>(n));
   }
 };
 
