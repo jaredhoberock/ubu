7bc7f0c6bc7705dcce097f37041aa3e563fadd93
Jared Hoberock <jaredhoberock@gmail.com>
2022-05-04 20:11:05 -0500

    Introduce detail::has_exceptions

--- aspera/detail/reflection.hpp
+++ aspera/detail/reflection.hpp
@@ -3,6 +3,7 @@
 #include "prologue.hpp"
 
 #include "reflection/has_cuda_runtime.hpp"
+#include "reflection/has_exceptions.hpp"
 #include "reflection/is_device.hpp"
 #include "reflection/is_host.hpp"
 
