38fcc2a1d7e6549dcdba2337268895accb232fb8
Jared Hoberock <jaredhoberock@gmail.com>
2022-05-31 12:36:25 -0500

    Add CUDA header

---
+++ ubu/cuda.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "detail/prologue.hpp"
+
+#include "cuda/callback_executor.hpp"
+#include "cuda/device_memory_resource.hpp"
+#include "cuda/device_ptr.hpp"
+#include "cuda/event.hpp"
+#include "cuda/kernel_executor.hpp"
+#include "cuda/managed_memory_resource.hpp"
+#include "cuda/thread_id.hpp"
+
+#include "detail/epilogue.hpp"
+
