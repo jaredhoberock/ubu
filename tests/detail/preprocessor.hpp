f529060cbb483a6e036fb88100f46f171d6a6b36
Jared Hoberock <jaredhoberock@gmail.com>
2022-06-01 16:29:51 -0500

    Eliminate superfluous tests

--- tests/detail/preprocessor.hpp
+++ tests/detail/preprocessor.hpp
@@ -4,58 +4,5 @@
 #error UBU_INCLUDE_LEVEL defined in client code.
 #endif
 
-#ifdef UBU_ANNOTATION
-#error UBU_ANNOTATION defined in client code.
-#endif
-
-#ifdef UBU_CONCATENATE
-#error UBU_CONCATENATE defined in client code.
-#endif
-
-#ifdef UBU_CONCATENATE_IMPL
-#error UBU_CONCATENATE_IMPL defined in client code.
-#endif
-
-#ifdef UBU_DETAIL_NAMESPACE
-#error UBU_DETAIL_NAMESPACE defined in client code.
-#endif
-
-#ifdef UBU_DETAIL_NAMESPACE_NEEDS_UNDEF
-#error UBU_DETAIL_NAMESPACE_NEEDS_UNDEF defined in client code.
-#endif
-
-#ifdef UBU_EXEC_CHECK_DISABLE
-#error UBU_EXEC_CHECK_DISABLE defined in client code.
-#endif
-
-#ifdef UBU_HAS_CUDART
-#error UBU_HAS_CUDART defined in client code.
-#endif
-
-#ifdef UBU_HAS_EXCEPTIONS
-#error UBU_HAS_EXCEPTIONS defined in client code.
-#endif
-
-#ifdef UBU_MAKE_UNIQUE
-#error UBU_MAKE_UNIQUE defined in client code.
-#endif
-
-#ifdef UBU_REQUIRES
-#error UBU_REQUIRES defined in client code.
-#endif
-
-#ifdef UBU_REQUIRES_IMPL
-#error UBU_REQUIRES_IMPL defined in client code.
-#endif
-
-#ifdef UBU_REQUIRES_DEF
-#error UBU_REQUIRES_DEF defined in client code.
-#endif
-
-#ifdef UBU_REQUIRES_DEF_IMPL
-#error UBU_REQUIRES_DEF_IMPL defined in client code.
-#endif
-
-
 void test_preprocessor() {}
 
