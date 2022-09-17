8fa214b8cce4822a8d1c99d47a3f06fa0cb5fc02
Jared Hoberock <jaredhoberock@gmail.com>
2022-09-17 16:21:51 -0500

    Introduce cuda::managed_allocator

---
+++ tests/platform/cuda/managed_allocator.hpp
@@ -0,0 +1,37 @@
+#include <ubu/causality/first_cause.hpp>
+#include <ubu/memory/allocator/allocator.hpp>
+#include <ubu/platform/cuda/kernel_executor.hpp>
+#include <ubu/platform/cuda/managed_allocator.hpp>
+
+namespace ns = ubu;
+
+void test_concepts()
+{
+  static_assert(ns::allocator<ns::cuda::managed_allocator<int>>);
+}
+
+template<class T>
+void test_allocation()
+{
+  using namespace ns::cuda;
+
+  cudaStream_t stream = 0;
+  kernel_executor ex{0,stream};
+  managed_allocator<T> alloc{ex.device()};
+
+  {
+    // test allocation and deletion
+
+    T* ptr = alloc.allocate(1);
+
+    alloc.deallocate(ptr, 1);
+  }
+}
+
+void test_managed_allocator()
+{
+  test_concepts();
+  test_allocation<char>();
+  test_allocation<int>();
+}
+
---
+++ ubu/platform/cuda/managed_allocator.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "../../detail/prologue.hpp"
+
+#include "managed_memory_resource.hpp"
+#include <cuda_runtime_api.h>
+
+namespace ubu::cuda
+{
+
+
+template<class T>
+class managed_allocator : private managed_memory_resource
+{
+  private:
+    using super_t = managed_memory_resource;
+
+  public:
+    using value_type = T;
+
+    explicit managed_allocator(int device)
+      : super_t{device}
+    {}
+
+    managed_allocator()
+      : managed_allocator{0}
+    {}
+
+    managed_allocator(const managed_allocator&) = default;
+
+    template<class OtherU>
+    managed_allocator(const managed_allocator<OtherU>& other)
+      : managed_allocator{other.device()}
+    {}
+
+    T* allocate(std::size_t n) const
+    {
+      return reinterpret_cast<T*>(super_t::allocate(sizeof(T) * n));
+    }
+
+    void deallocate(T* ptr, std::size_t n) const
+    {
+      super_t::deallocate(ptr, sizeof(T) * n);
+    }
+
+    int device() const
+    {
+      return super_t::device();
+    }
+
+    bool operator==(const managed_allocator& other) const
+    {
+      return super_t::operator==(other);
+    }
+
+    inline bool operator!=(const managed_allocator& other) const
+    {
+      return !(*this == other);
+    }
+};
+
+
+} // end ubu::cuda
+
+
+#include "../../detail/epilogue.hpp"
+
