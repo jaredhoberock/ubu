e8871ee8b7612f49272f56bb46bb737e24f8bdc6
Jared Hoberock <jaredhoberock@gmail.com>
2022-11-16 16:02:14 -0600

    Test that cuda::thread_id is tuple_like

--- tests/platform/cuda/thread_id.hpp
+++ tests/platform/cuda/thread_id.hpp
@@ -1,5 +1,6 @@
 #include <ubu/coordinate/congruent.hpp>
 #include <ubu/coordinate/coordinate.hpp>
+#include <ubu/coordinate/detail/tuple_algorithm.hpp>
 #include <ubu/coordinate/weakly_congruent.hpp>
 #include <ubu/platform/cuda/thread_id.hpp>
 
@@ -10,5 +11,6 @@ void test_thread_id()
   static_assert(ns::coordinate<ns::cuda::thread_id>);
   static_assert(ns::weakly_congruent<ns::cuda::thread_id, ns::cuda::thread_id>);
   static_assert(ns::congruent<ns::cuda::thread_id, ns::cuda::thread_id>);
+  static_assert(ns::detail::tuple_like<ns::cuda::thread_id>);
 }
 
