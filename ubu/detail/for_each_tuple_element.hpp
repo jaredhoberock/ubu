1712e64ec090e2318425d765d2e9c513021cfc8c
Jared Hoberock <jaredhoberock@gmail.com>
2022-11-16 15:59:58 -0600

    Avoid using std::apply, which doesn't work with all tuple-like types

--- ubu/detail/for_each_tuple_element.hpp
+++ ubu/detail/for_each_tuple_element.hpp
@@ -4,6 +4,7 @@
 
 #include <functional>
 #include <tuple>
+#include <utility>
 
 
 namespace ubu::detail
@@ -14,15 +15,22 @@ template<class... Args>
 constexpr void ignore_arguments(Args&&...){}
 
 
+template<class F, class T, std::size_t... I>
+constexpr void for_each_tuple_element_impl(F&& f, T&& t, std::index_sequence<I...>)
+{
+  // the business with the trailing comma zero avoids problems with void-returning f
+  ignore_arguments(
+    (std::invoke(std::forward<F>(f), get<I>(std::forward<T>(t))), 0)...
+  );
+}
+
+
 template<class F, class Tuple>
 constexpr void for_each_tuple_element(F&& f, Tuple&& t)
 {
-  std::apply([&](auto&&... elements)
-  {
-    // call std::invoke on f with each elements and unpack as parameters into ignore_arguments
-    // use the comma operator to avoid problems with f returning void
-    detail::ignore_arguments((std::invoke(std::forward<F>(f), elements), 0)...);
-  }, std::forward<Tuple>(t));
+  constexpr std::size_t n = std::tuple_size_v<std::remove_cvref_t<Tuple>>;
+
+  for_each_tuple_element_impl(std::forward<F>(f), std::forward<Tuple>(t), std::make_index_sequence<n>());
 }
 
 
