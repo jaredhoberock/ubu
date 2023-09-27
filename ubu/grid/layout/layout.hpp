3f07b0d6d5dd35e0181b1356237fbaa606b171e2
Jared Hoberock <jaredhoberock@gmail.com>
2023-09-27 15:01:45 -0500

    Introduce layout_for and layout_onto

--- ubu/grid/layout/layout.hpp
+++ ubu/grid/layout/layout.hpp
@@ -3,6 +3,8 @@
 #include "../../detail/prologue.hpp"
 
 #include "../grid.hpp"
+#include "../coordinate/congruent.hpp"
+#include "../coordinate/coordinate.hpp"
 
 namespace ubu
 {
@@ -11,6 +13,26 @@ template<class T>
 concept layout =
   grid<T>
   and coordinate<grid_element_t<T>>
+;
+
+template<class L, class Coord>
+concept layout_for =
+  layout<L>
+  and coordinate<Coord>
+  and requires(L layout, Coord coord)
+  {
+    { layout[coord] } -> coordinate;
+  }
+;
+
+template<class L, class FromCoord, class ToCoord>
+concept layout_onto =
+  layout_for<L, FromCoord>
+  and coordinate<ToCoord>
+  and requires(L layout, FromCoord coord)
+  {
+    { layout[coord] } -> congruent<ToCoord>;
+  }
 ;
 
 } // end ubu
