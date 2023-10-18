dc861b00ddf65624385a1e08f49a32ea635bce2b
Jared Hoberock <jaredhoberock@gmail.com>
2023-10-18 13:18:29 -0500

    Add ostream and libfmt support for underscore

--- ubu/grid/slice/underscore.hpp
+++ ubu/grid/slice/underscore.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../detail/prologue.hpp"
+#include <iostream>
 #include <type_traits>
 
 namespace ubu
@@ -10,6 +11,11 @@ namespace detail
 
 struct underscore_t {};
 
+std::ostream& operator<<(std::ostream& os, underscore_t)
+{
+  return os << "_";
+}
+
 template<class T>
 constexpr bool is_underscore_v = std::is_same_v<std::remove_cvref_t<T>,underscore_t>;
 
@@ -18,6 +24,31 @@ constexpr bool is_underscore_v = std::is_same_v<std::remove_cvref_t<T>,underscor
 constexpr detail::underscore_t _;
 
 } // end ubu
+
+#if __has_include(<fmt/core.h>)
+#include <fmt/core.h>
+
+namespace fmt
+{
+
+template<>
+struct formatter<ubu::detail::underscore_t>
+{
+  constexpr auto parse(format_parse_context& ctx)
+  {
+    return ctx.begin();
+  }
+
+  template<class FormatContext>
+  auto format(ubu::detail::underscore_t, FormatContext& ctx)
+  {
+    return format_to(ctx.out(), "_");
+  }
+};
+
+} // end fmt
+
+#endif // libfmt
 
 #include "../../detail/epilogue.hpp"
 
