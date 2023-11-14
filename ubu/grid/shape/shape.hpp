9734e5262985efda9c47abd0434790b10e832fd1
Jared Hoberock <jaredhoberock@gmail.com>
2023-11-14 13:56:50 -0600

    Allow shape CPO to prefer member variables named "shape" with high priority

--- ubu/grid/shape/shape.hpp
+++ ubu/grid/shape/shape.hpp
@@ -4,12 +4,27 @@
 
 #include "../coordinate.hpp"
 #include <ranges>
+#include <type_traits>
 
 namespace ubu
 {
 namespace detail
 {
 
+template<class T>
+concept has_shape_member_variable = requires(T arg)
+{
+// XXX WAR circle bug
+#if defined(__circle_lang__)
+  arg.shape;
+  requires requires(decltype(arg.shape) s)
+  {
+    { s } -> ubu::coordinate;
+  };
+#else
+  { arg.shape } -> coordinate;
+#endif
+};
 
 template<class T>
 concept has_shape_member_function = requires(T arg)
@@ -33,14 +48,23 @@ concept has_ranges_size = requires(T arg)
 struct dispatch_shape
 {
   template<class T>
-    requires has_shape_member_function<T&&>
+    requires has_shape_member_variable<T&&>
+  constexpr coordinate auto operator()(T&& arg) const
+  {
+    return std::forward<T>(arg).shape;
+  }
+
+  template<class T>
+    requires (not has_shape_member_variable<T&&>
+              and has_shape_member_function<T&&>)
   constexpr coordinate auto operator()(T&& arg) const
   {
     return std::forward<T>(arg).shape();
   }
 
   template<class T>
-    requires (not has_shape_member_function<T&&>
+    requires (not has_shape_member_variable<T&&>
+              and not has_shape_member_function<T&&>
               and has_shape_free_function<T&&>)
   constexpr coordinate auto operator()(T&& arg) const
   {
@@ -48,7 +72,8 @@ struct dispatch_shape
   }
 
   template<class T>
-    requires (not has_shape_member_function<T&&>
+    requires (not has_shape_member_variable<T&&>
+              and not has_shape_member_function<T&&>
               and not has_shape_free_function<T&&>
               and has_ranges_size<T&&>)
   constexpr coordinate auto operator()(T&& arg) const
