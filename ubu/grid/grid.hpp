cdf2240557b2c7064a2a7c9ee08abfae4af4f665
Jared Hoberock <jaredhoberock@gmail.com>
2023-11-15 12:45:18 -0600

    Introduce grid_reference_t

--- ubu/grid/grid.hpp
+++ ubu/grid/grid.hpp
@@ -34,11 +34,16 @@ concept grid =
   and indexable_by<T, shape_t<T>>
 ;
 
-template<grid T>
-using grid_element_t = std::remove_cvref_t<decltype(std::declval<T>()[std::declval<shape_t<T>>()])>;
-
+// XXX it might be more consistent with the allocator/executor stuff if we named this
+//     grid_coordinate_t instead of grid_shape_t, especially considering we already have the name shape_t
 template<grid T>
 using grid_shape_t = shape_t<T>;
+
+template<grid T>
+using grid_reference_t = decltype(std::declval<T>()[std::declval<grid_shape_t<T>>()]);
+
+template<grid T>
+using grid_element_t = std::remove_cvref_t<grid_reference_t<T>>;
 
 template<class G, class T>
 concept grid_of =
