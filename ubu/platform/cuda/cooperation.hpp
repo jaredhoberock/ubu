92b15de79813fc0d2c9a94728720c5e8ac3087c0
Jared Hoberock <jaredhoberock@gmail.com>
2023-12-13 17:01:43 -0600

    Introduce cuda::descend_with_group_coord overload for block_like

--- ubu/platform/cuda/cooperation.hpp
+++ ubu/platform/cuda/cooperation.hpp
@@ -4,9 +4,14 @@
 
 #include "../../detail/reflection.hpp"
 #include "../../memory/buffer/empty_buffer.hpp"
+#include "../../cooperation/cooperator/basic_cooperator.hpp"
+#include "../../cooperation/cooperator/concepts/cooperator.hpp"
+#include "../../cooperation/cooperator/cooperator_thread_scope.hpp"
 #include <cstddef>
 #include <span>
 #include <string_view>
+#include <utility>
+
 
 namespace ubu::cuda
 {
@@ -78,6 +83,33 @@ struct device_workspace
 #endif
   }
 };
+
+
+template<class C>
+concept warp_like =
+  cooperator<C>
+  and cooperator_thread_scope_v<C> == "warp"
+;
+
+template<class C>
+concept block_like =
+  cooperator<C>
+  and cooperator_thread_scope_v<C> == "block"
+;
+
+// overload descend_with_group_coord for 1D block_like groups
+// this allows us to get a warp from a block which doesn't happen
+// to have a hierarchical workspace
+// returns the pair (warp_cooperator, which_warp)
+template<block_like B>
+  requires (rank_v<shape_t<B>> == 1)
+constexpr auto descend_with_group_coord(B block)
+{
+  constexpr int warp_size = 32;
+  int lane = coord(block) % warp_size;
+  int which_warp = coord(block) / warp_size;
+  return std::pair(basic_cooperator(lane, warp_size, warp_workspace{}), which_warp);
+}
 
 
 } // end ubu::cuda
