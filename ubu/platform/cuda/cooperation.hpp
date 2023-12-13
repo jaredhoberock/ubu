bcd93a52a8f5d1ad1adbcd9da84cd216b793c6f2
Jared Hoberock <jaredhoberock@gmail.com>
2023-12-13 17:27:55 -0600

    Introduce cuda::coop_reduce for warp_like

--- ubu/platform/cuda/cooperation.hpp
+++ ubu/platform/cuda/cooperation.hpp
@@ -7,9 +7,13 @@
 #include "../../cooperation/cooperator/basic_cooperator.hpp"
 #include "../../cooperation/cooperator/concepts/cooperator.hpp"
 #include "../../cooperation/cooperator/cooperator_thread_scope.hpp"
+#include <cmath>
+#include <concepts>
 #include <cstddef>
+#include <optional>
 #include <span>
 #include <string_view>
+#include <type_traits>
 #include <utility>
 
 
@@ -109,6 +113,135 @@ constexpr auto descend_with_group_coord(B block)
   int lane = coord(block) % warp_size;
   int which_warp = coord(block) / warp_size;
   return std::pair(basic_cooperator(lane, warp_size, warp_workspace{}), which_warp);
+}
+
+
+namespace detail
+{
+
+
+template<std::integral T>
+constexpr T ceil_div(const T& dividend, const T& divisor)
+{
+  return (dividend + divisor - 1) / divisor;
+}
+
+
+template<class T>
+concept plain_old_data = std::is_trivial_v<T> and std::is_standard_layout_v<T>;
+
+
+template<plain_old_data T>
+constexpr T shuffle_down(const T& x, int offset, int width)
+{ 
+#if defined(__CUDACC__)
+  constexpr std::size_t num_words = ceil_div(sizeof(T), sizeof(int));
+
+  union
+  {
+    int words[num_words];
+    T value;
+  } u;
+  u.value = x;
+
+  for(int i = 0; i < num_words; ++i)
+  {
+    u.words[i] = __shfl_down_sync(__activemask(), u.words[i], offset, width);
+  }
+
+  return u.value;
+#else
+  return {};
+#endif
+}
+
+
+template<plain_old_data T>
+constexpr std::optional<T> shuffle_down(const std::optional<T>& x, int offset, int width)
+{
+#if defined(__CUDACC__)
+  constexpr std::size_t num_words = ceil_div(sizeof(T), sizeof(int));
+
+  union
+  {
+    int words[num_words];
+    T value;
+  } u;
+
+  if(x)
+  {
+    u.value = *x;
+  }
+
+  for(int i= 0; i < num_words; ++i)
+  {
+    u.words[i] = __shfl_down_sync(__activemask(), u.words[i], offset, width);
+  }
+
+  // communicate whether or not the words we shuffled came from a valid object
+  bool is_valid = x ? true : false;
+  is_valid = __shfl_down_sync(__activemask(), is_valid, offset, width);
+
+  return is_valid ? std::make_optional(u.value) : std::nullopt;
+#else
+  return {};
+#endif
+}
+
+
+} // end detail
+
+
+template<warp_like W, class T, class F>
+constexpr std::optional<T> coop_reduce(W self, std::optional<T> value, int num_values, F binary_op)
+{
+  // XXX we need to be able to say this in a constexpr
+  // constexpr int num_threads = size(self);
+  constexpr int warp_size = 32;
+  constexpr int num_threads = warp_size;
+  constexpr int num_passes = 5; // this is log2(warp_size)
+
+  if(num_values == num_threads)
+  {
+    for(int pass = 0; pass != num_passes; ++pass)
+    {
+      int offset = 1 << pass;
+      T other = detail::shuffle_down(*value, offset, num_threads);
+      value = binary_op(*value, other);
+    }
+  }
+  else
+  {
+    for(int pass = 0; pass != num_passes; ++pass)
+    {
+      int offset = 1 << pass;
+      std::optional other = detail::shuffle_down(value, offset, num_threads);
+      if((id(self) + offset < num_values) and other) *value = binary_op(*value, *other);
+    }
+  }
+
+  return (id(self) == 0) ? value : std::nullopt;
+}
+
+
+namespace detail
+{
+
+
+template<warp_like W, class T>
+constexpr int coop_count_values(W, const std::optional<T>& maybe_value)
+{
+  return __popc(__ballot_sync(0xFFFFFFFF, maybe_value.has_value()));
+}
+
+
+} // end detail
+
+
+template<warp_like W, class T, class F>
+constexpr std::optional<T> coop_reduce(W self, const std::optional<T>& value, F binary_op)
+{
+  return coop_reduce(self, value, detail::coop_count_values(self, value), binary_op);
 }
 
 
