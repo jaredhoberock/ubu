bd1dcd7a40b959338fa4c61482d5de51cb00ddfc
Jared Hoberock <jaredhoberock@gmail.com>
2024-01-03 15:11:09 -0600

    Tweak some names

--- ubu/grid/coordinate/element.hpp
+++ ubu/grid/coordinate/element.hpp
@@ -20,10 +20,10 @@ namespace ubu::detail
 
 
 template<std::size_t i, class T>
-concept has_element_member_function = requires(T obj) { obj.template element<i>(); };
+concept has_element_member_function_template = requires(T obj) { obj.template element<i>(); };
 
 template<std::size_t i, class T>
-concept has_element_free_function = requires(T obj) { element<i>(obj); };
+concept has_element_free_function_template = requires(T obj) { element<i>(obj); };
 
 template<class T, class Arg>
 concept has_operator_bracket = requires(T obj, Arg arg) { obj[arg]; };
@@ -37,7 +37,7 @@ struct dispatch_element
 {
   // when T has a member function template arg.element<i>(), return that
   template<class T>
-    requires has_element_member_function<i,T&&>
+    requires has_element_member_function_template<i,T&&>
   constexpr auto operator()(T&& arg) const
     -> decltype(std::forward<T>(arg).template element<i>())
   {
@@ -46,8 +46,8 @@ struct dispatch_element
 
   // else, when T has a free function template element<i>(arg), return that
   template<class T>
-    requires(!has_element_member_function<i,T&&> and
-              has_element_free_function<i,T&&>)
+    requires(!has_element_member_function_template<i,T&&> and
+              has_element_free_function_template<i,T&&>)
   constexpr auto operator()(T&& arg) const
     -> decltype(element<i>(std::forward<T>(arg)))
   {
@@ -56,8 +56,8 @@ struct dispatch_element
 
   // else, when T is just a number and the index i is zero, just return the number
   template<class T>
-    requires(!has_element_free_function<i,T&&> and 
-             !has_element_free_function<i,T&&> and
+    requires(!has_element_free_function_template<i,T&&> and 
+             !has_element_free_function_template<i,T&&> and
              number<std::remove_cvref_t<T&&>> and
              i == 0)
   constexpr T&& operator()(T&& num) const
@@ -67,8 +67,8 @@ struct dispatch_element
 
   // else, when T has get<i>(arg), return that
   template<class T>
-    requires(!has_element_free_function<i,T&&> and 
-             !has_element_free_function<i,T&&> and
+    requires(!has_element_free_function_template<i,T&&> and 
+             !has_element_free_function_template<i,T&&> and
              !number<T&&> and
              has_get<i,T&&>)
   constexpr auto operator()(T&& arg) const
@@ -79,8 +79,8 @@ struct dispatch_element
 
   // else, when T has operator bracket obj[i], return that
   template<class T>
-    requires(!has_element_free_function<i,T&&> and 
-             !has_element_free_function<i,T&&> and
+    requires(!has_element_free_function_template<i,T&&> and 
+             !has_element_free_function_template<i,T&&> and
              !number<T&&> and
              !has_get<i,T&&> and
              has_operator_bracket<T&&,std::size_t>)
