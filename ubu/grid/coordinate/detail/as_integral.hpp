eceb1ef7a740bbf6ed85cc61f9b0226c91710af0
Jared Hoberock <jaredhoberock@gmail.com>
2024-01-04 17:58:47 -0600

    Avoid using scalar_coordinate concept in as_integral

--- ubu/grid/coordinate/detail/as_integral.hpp
+++ ubu/grid/coordinate/detail/as_integral.hpp
@@ -2,21 +2,36 @@
 
 #include "../../../detail/prologue.hpp"
 
-#include "../concepts/coordinate.hpp"
+#include "../detail/tuple_algorithm.hpp"
+#include <concepts>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
 namespace ubu::detail
 {
 
-template<scalar_coordinate C>
+template<class T>
+concept single_integral = 
+  tuple_like_of_size<T,1>
+  and std::integral<std::remove_cvref_t<std::tuple_element_t<0,std::remove_cvref_t<T>>>>
+;
+
+template<class C>
+  requires (std::integral<std::remove_cvref_t<C>> or single_integral<C>)
 constexpr decltype(auto) as_integral(C&& coord)
 {
-  if constexpr (std::integral<std::remove_cvref_t<C>>) return std::forward<C>(coord);
-  else return get<0>(std::forward<C>(coord));
+  if constexpr(tuple_like<C>)
+  {
+    return get<0>(std::forward<C>(coord));
+  }
+  else
+  {
+    return std::forward<C>(coord);
+  }
 }
 
-template<scalar_coordinate C>
+template<class C>
 using as_integral_t = std::remove_cvref_t<decltype(as_integral(std::declval<C>()))>;
 
 } // end ubu::detail
