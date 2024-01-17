54d9c2e460ed585d34c353aad10357ad17d38cd1
Jared Hoberock <jaredhoberock@gmail.com>
2024-01-17 13:36:47 -0600

    Add a template parameter to strided_layout to control the type of the result

--- ubu/grid/layout/strided_layout.hpp
+++ ubu/grid/layout/strided_layout.hpp
@@ -11,25 +11,34 @@
 #include "detail/strided_layout_compose_impl.hpp"
 #include "layout.hpp"
 #include "stride/apply_stride.hpp"
+#include "stride/apply_stride_r.hpp"
 #include "stride/compact_column_major_stride.hpp"
 #include "stride/stride_for.hpp"
 #include <concepts>
+#include <type_traits>
 
 
 namespace ubu
 {
 
 
-template<coordinate S, stride_for<S> D = S>
+template<coordinate S, stride_for<S> D = S, coordinate R = apply_stride_t<D,S>>
+  requires (congruent<R,apply_stride_t<D,S>>
+            and not std::is_reference_v<R>)
 class strided_layout
 {
   public:
-    constexpr strided_layout(S s, D d)
-      : shape_{s}, stride_{d}
+    constexpr strided_layout(S shape, D stride)
+      : shape_{shape}, stride_{stride}
     {}
 
-    constexpr strided_layout(S s)
-      : strided_layout(s, compact_column_major_stride(s))
+    // this ctor is provided for CTAD. The coshape parameter is ignored otherwise.
+    constexpr strided_layout(S shape, D stride, R /*coshape*/)
+      : strided_layout{shape,stride}
+    {}
+
+    constexpr strided_layout(S shape)
+      : strided_layout(shape, compact_column_major_stride(shape))
     {}
 
     strided_layout(const strided_layout&) = default;
@@ -41,15 +50,9 @@ class strided_layout
     {}
 
     template<weakly_congruent<S> C>
-    constexpr coordinate auto apply_layout(const C& coord) const
+    constexpr R operator[](const C& coord) const
     {
-      return apply_stride(stride(), colexicographical_lift(coord, shape()));
-    }
-
-    template<weakly_congruent<S> C>
-    constexpr coordinate auto operator[](const C& coord) const
-    {
-      return apply_layout(coord);
+      return apply_stride_r<R>(stride(), colexicographical_lift(coord, shape()));
     }
 
     constexpr S shape() const
@@ -69,25 +72,27 @@ class strided_layout
 
     // XXX consider whether the following functions need to be members
 
-    // XXX the return type of this should be the same as operator[]
-    constexpr coordinate auto coshape() const
+    constexpr R coshape() const
     {
-      auto last_position = apply_layout(size() - 1);
+      R last_position = operator[](size() - 1);
       return coordinate_sum(last_position, ones<decltype(last_position)>);
     }
 
     // XXX the return type of this should be constrained to layout
-    template<coordinate S1, stride_for<S1> D1>
-    constexpr auto compose(const strided_layout<S1,D1>& other) const
+    template<coordinate S1, stride_for<S1> D1, coordinate R1>
+    constexpr auto compose(const strided_layout<S1,D1,R1>& other) const
     {
       auto [s,d] = detail::strided_layout_compose_impl(shape(), stride(), other.shape(), other.stride());
-      return make_strided_layout(s,d);
+      return make_strided_layout_r<R>(s,d);
     }
 
     // XXX the return type of this should be constrained to layout
-    template<coordinate... Ss, stride_for<Ss>... Ds>
-    constexpr auto concatenate(const strided_layout<Ss,Ds>&... layouts) const
+    template<coordinate... Ss, stride_for<Ss>... Ds, coordinate... Rs>
+    constexpr auto concatenate(const strided_layout<Ss,Ds,Rs>&... layouts) const
     {
+      // XXX what should be the resulting layout's result type?
+      //     some concatenation of the Rs?
+      
       using shape_tuple = std::conditional_t<detail::tuple_like<S>, S, ubu::int1>;
       using stride_tuple = std::conditional_t<detail::tuple_like<D>, D, ubu::int1>;
 
@@ -99,6 +104,8 @@ class strided_layout
     template<std::integral I>
     constexpr auto complement(I cosize_hi) const
     {
+      // XXX what should be the resulting layout's result type? I?
+      
       auto [s,d] = detail::strided_layout_complement_impl(shape(), stride(), cosize_hi);
       return make_strided_layout(s,d);
     }
@@ -115,7 +122,7 @@ class strided_layout
     {
       auto result_shape = slice_coordinate(shape(), katana);
       auto result_stride = slice_coordinate(stride(), katana);
-      return make_strided_layout(result_shape, result_stride);
+      return make_strided_layout_r<R>(result_shape, result_stride);
     }
 
     // XXX the return type of this is some type of strided_layout
@@ -124,12 +131,19 @@ class strided_layout
     {
       auto result_shape = dice_coordinate(shape(), katana);
       auto result_stride = dice_coordinate(stride(), katana);
-      return make_strided_layout(result_shape, result_stride);
+      return make_strided_layout_r<R>(result_shape, result_stride);
     }
 
   private:
     template<class S1, stride_for<S1> D1>
     constexpr static strided_layout<S1,D1> make_strided_layout(S1 s, D1 d) 
+    {
+      return {s, d};
+    }
+
+    template<class R1, class S1, stride_for<S1> D1>
+      requires congruent<R,apply_stride_t<D1,S1>>
+    constexpr static strided_layout<S1,D1,R1> make_strided_layout_r(S1 s, D1 d)
     {
       return {s, d};
     }
