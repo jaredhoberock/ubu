1aed840ccfabb9c19cc275dc2e9e0ccc1c5ba7c3
Jared Hoberock <jaredhoberock@gmail.com>
2024-01-17 16:02:44 -0600

    Simplify the implementation of zeros<T>

--- ubu/grid/coordinate/zeros.hpp
+++ ubu/grid/coordinate/zeros.hpp
@@ -9,37 +9,10 @@
 
 namespace ubu
 {
-namespace detail
-{
 
-
-template<scalar_coordinate T>
-constexpr T zeros_impl()
-{
-  return 0;
-}
-
-
-template<nonscalar_coordinate T>
-constexpr T zeros_impl()
-{
-  T result{};
-
-  detail::for_each_tuple_element([](auto& element)
-  {
-    element = zeros_impl<std::remove_cvref_t<decltype(element)>>();
-  }, result);
-
-  return result;
-}
-
-
-} // end detail
-
-
+// XXX this definition assumes that brace-initializing T will actually put a 0 in each mode of T
 template<coordinate T>
-constexpr std::remove_cvref_t<T> zeros = detail::zeros_impl<std::remove_cvref_t<T>>();
-
+constexpr std::remove_cvref_t<T> zeros{};
 
 } // end ubu
 
