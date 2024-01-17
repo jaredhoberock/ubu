41f1e2c3e578e2567e2a4f128b48a403bf86682b
Jared Hoberock <jaredhoberock@gmail.com>
2024-01-17 14:50:59 -0600

    Introduce grid_coordinate_t

--- ubu/grid/grid.hpp
+++ ubu/grid/grid.hpp
@@ -10,31 +10,56 @@
 
 namespace ubu
 {
+namespace detail
+{
+
+template<class T>
+struct coordinate_or_shape
+{
+  using type = shape_t<T>;
+};
+
+template<class T>
+  requires requires { typename std::remove_cvref_t<T>::coordinate_type; }
+struct coordinate_or_shape<T>
+{
+  using type = typename std::remove_cvref_t<T>::coordinate_type;
+};
+
+// coordinate_or_shape returns T::coordinate_type if it exists; otherwise, shape_t<T>
+template<class T>
+using coordinate_or_shape_t = typename coordinate_or_shape<T>::type;
+
+} // end detail
+
 
 template<class T>
 concept grid =
+  // we must be able to get the shape of a grid
   requires(T g)
   {
     shape(g);
   }
-  and coordinate_for<shape_t<T>, T>
-  and requires(T g, shape_t<T> c)
+
+  // the grid's coordinate and shape types must be congruent
+  and congruent<detail::coordinate_or_shape_t<T>, shape_t<T>>
+
+  // we must be able to access the grid element at a coordinate
+  and requires(T g, detail::coordinate_or_shape_t<T> c)
   {
-    element_exists(g, c);
+    element(g, c);         // we must be able to get the grid element at c
+    element_exists(g, c);  // we must be able to check whether the grid element at c exists
   }
 ;
-
-// XXX in addition to grid_shape_t, I think we also need grid_coordinate_t
-//     in some important cases, the coordinate type will differ from the shape type
-//     for example, grids whose shape is known to be a constant at compile time
-//     would have a grid_shape_t like std::integral_constant, while their coordinate
-//     type would still be able to vary dynamically
 
 template<grid T>
 using grid_shape_t = shape_t<T>;
 
 template<grid T>
-using grid_reference_t = decltype(std::declval<T>()[std::declval<grid_shape_t<T>>()]);
+using grid_coordinate_t = detail::coordinate_or_shape_t<T>;
+
+template<grid T>
+using grid_reference_t = decltype(std::declval<T>()[std::declval<grid_coordinate_t<T>>()]);
 
 template<grid T>
 using grid_element_t = std::remove_cvref_t<grid_reference_t<T>>;
