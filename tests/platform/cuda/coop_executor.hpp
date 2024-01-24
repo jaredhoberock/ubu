8e15428412a9f0085960af9ac124268427ef9a77
Jared Hoberock <jaredhoberock@gmail.com>
2024-01-24 16:01:54 -0600

    Introduce and test cuda::coop_executor

---
+++ tests/platform/cuda/coop_executor.hpp
@@ -0,0 +1,500 @@
+#include <array>
+#include <ubu/causality.hpp>
+#include <ubu/cooperation/workspace/get_local_workspace.hpp>
+#include <ubu/execution/executor/bulk_execute_after.hpp>
+#include <ubu/execution/executor/bulk_execute_with_workspace_after.hpp>
+#include <ubu/execution/executor/concepts/executor.hpp>
+#include <ubu/execution/executor/execute_kernel.hpp>
+#include <ubu/execution/executor/finally_execute_after.hpp>
+#include <ubu/execution/executor/first_execute.hpp>
+#include <ubu/grid/coordinate/colexicographical_lift.hpp>
+#include <ubu/grid/layout/stride/apply_stride.hpp>
+#include <ubu/grid/layout/stride/compact_column_major_stride.hpp>
+#include <ubu/memory/buffer/reinterpret_buffer.hpp>
+#include <ubu/platform/cuda/coop_executor.hpp>
+
+#undef NDEBUG
+#include <cassert>
+
+namespace ns = ubu;
+
+
+void test_concepts()
+{
+  static_assert(ns::coordinate<ns::cuda::coop_executor::shape_type>);
+  static_assert(std::same_as<ns::cuda::coop_executor::shape_type, ns::executor_coordinate_t<ns::cuda::coop_executor>>);
+  static_assert(ns::executor<ns::cuda::coop_executor>);
+  static_assert(ns::hierarchical_workspace<ns::executor_workspace_t<ns::cuda::coop_executor>>);
+  static_assert(std::same_as<ns::int2, ns::executor_workspace_shape_t<ns::cuda::coop_executor>>);
+}
+
+
+void test_equality(ns::cuda::coop_executor ex1)
+{
+  using namespace ns;
+  
+  cuda::coop_executor ex2 = ex1;
+
+  assert(ex1 == ex2);
+  assert(!(ex1 != ex2));
+}
+
+
+#ifndef __managed__
+#define __managed__
+#endif
+
+
+__managed__ int result;
+__managed__ int result1;
+__managed__ int result2;
+
+
+void test_first_execute(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  result = 0;
+  int expected = 13;
+
+  try
+  {
+    auto e = ns::first_execute(ex, [expected]
+    {
+      result = expected;
+    });
+
+    ns::wait(e);
+    assert(expected == result);
+  }
+  catch(std::runtime_error&)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+void test_execute_after(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  result1 = 0;
+  int expected1 = 13;
+  
+  try
+  {
+    auto e1 = ns::first_execute(ex, [expected1]
+    {
+      result1 = expected1;
+    });
+
+    result2 = 0;
+    int expected2 = 7;
+    auto e2 = ns::execute_after(ex, e1, [expected1,expected2]
+    {
+      assert(expected1 == result1);
+      result2 = expected2;
+    });
+
+    ns::wait(e2);
+    assert(expected2 == result2);
+  }
+  catch(std::runtime_error&)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+void test_finally_execute_after(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  result1 = 0;
+  int expected1 = 13;
+  
+  try
+  {
+    auto e1 = ns::first_execute(ex, [expected1]
+    {
+      result1 = expected1;
+    });
+
+    result2 = 0;
+    int expected2 = 7;
+    ns::finally_execute_after(ex, e1, [expected1,expected2]
+    {
+      assert(expected1 == result1);
+      result2 = expected2;
+    });
+
+    assert(cudaStreamSynchronize(ex.stream()) == cudaSuccess);
+    assert(expected2 == result2);
+  }
+  catch(std::runtime_error&)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+int hash(ns::uint2 coord)
+{
+  return coord.x ^ coord.y;
+}
+
+
+// this array has 2 axes to match threadIdx + blockIdx
+constexpr std::array<int,2> array_shape = {2, 4};
+__managed__ int array[2][4] = {};
+
+
+void test_bulk_execute_after_member_function(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  // partition the array shape into the coop_executor's shape type
+  // such that nearby threads touch nearby addresses
+  
+  cuda::coop_executor::shape_type shape {array_shape[0], array_shape[1]};
+
+  try
+  {
+    cuda::event before = initial_happening(ex);
+
+    cuda::event e = ex.bulk_execute_after(before, shape, [=](ns::int2 coord)
+    {
+      // hash the coordinate and store the result in the array
+      array[coord.y][coord.x] = hash(coord);
+    });
+
+    wait(e);
+
+    for(auto coord : lattice(shape))
+    {
+      int expected = hash(coord);
+
+      int result = array[coord.y][coord.x];
+
+      assert(expected == result);
+    }
+  }
+  catch(std::runtime_error& e)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+void test_bulk_execute_after_customization_point(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  // partition the array shape into int2
+  ns::int2 shape
+  {
+    array_shape[1], // num threads
+    array_shape[0]  // num blocks
+  };
+
+  try
+  {
+    cuda::event before = initial_happening(ex);
+
+    cuda::event e = bulk_execute_after(ex, before, shape, [=](ns::int2 coord)
+    {
+      int i = apply_stride(compact_column_major_stride(shape), coord);
+      auto c = colexicographical_lift(i, array_shape);
+
+      array[c[0]][c[1]] = i;
+    });
+
+    wait(e);
+
+    for(int i = 0; i < shape_size(array_shape); ++i)
+    {
+      auto c = colexicographical_lift(i, array_shape);
+
+      assert(i == array[c[0]][c[1]]);
+    }
+  }
+  catch(std::runtime_error& e)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+void test_bulk_execute_with_workspace_after_member_function(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  // partition the array shape into the coop_executor's shape type
+  // such that nearby threads touch nearby addresses
+  int block_size = array_shape[1];
+  int num_blocks = array_shape[0];
+
+  cuda::coop_executor::shape_type shape(block_size, num_blocks);
+
+  // create local workspaces with block_size ints and a global workspace with grid_size ints
+  ns::int2 workspace_shape(block_size * sizeof(int), num_blocks * sizeof(int));
+
+  try
+  {
+    cuda::event before = initial_happening(ex);
+
+    cuda::event e = ex.bulk_execute_with_workspace_after(before, shape, workspace_shape, [=](ns::int2 coord, cuda::coop_executor::workspace_type ws)
+    {
+      // hash the coordinate and store the result in the array
+      int result = hash(coord);
+      array[coord.y][coord.x] = result;
+
+      auto [thread_id, block_id] = coord;
+
+      // test the outer workspace
+      {
+        std::span<int> block_indices = reinterpret_buffer<int>(get_buffer(ws));
+
+        // the leader of each block records its block index in the global workspace
+        if(thread_id == 0)
+        {
+          block_indices[block_id] = block_id;
+        }
+
+        // use the global barrier
+        arrive_and_wait(get_barrier(ws));
+
+        // the first thread of the first block checks that each block was recorded in the global workspace
+        if(thread_id == 0 and block_id == 0)
+        {
+          int expected = 0;
+          for(int id : block_indices)
+          {
+            assert(expected == id);
+            ++expected;
+          }
+        }
+      }
+
+      // test the local workspace
+      {
+        std::span<int> thread_indices = reinterpret_buffer<int>(get_buffer(get_local_workspace(ws)));
+
+        // each thread records its index in the local workspace
+        thread_indices[thread_id] = thread_id;
+
+        // use the local barrier
+        arrive_and_wait(get_barrier(get_local_workspace(ws)));
+
+        // the first thread of each block checks that each thread was recorded in the local workspace
+        if(thread_id == 0)
+        {
+          int expected = 0;
+          for(int id : thread_indices)
+          {
+            assert(expected == id);
+            ++expected;
+          }
+        }
+      }
+    });
+
+    wait(e);
+
+    for(auto coord : lattice(shape))
+    {
+      int expected = hash(coord);
+      int result = array[coord.y][coord.x];
+      assert(expected == result);
+    }
+  }
+  catch(std::runtime_error& e)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+void test_bulk_execute_with_workspace_after_customization_point(ns::cuda::coop_executor ex)
+{
+  using namespace ns;
+
+  // partition the array shape into the coop_executor's shape type
+  // such that nearby threads touch nearby addresses
+  int block_size = array_shape[1];
+  int num_blocks = array_shape[0];
+
+  cuda::coop_executor::shape_type shape(block_size, num_blocks);
+
+  // create local workspaces with block_size ints and a global workspace with grid_size ints
+  ns::int2 workspace_shape(block_size * sizeof(int), num_blocks * sizeof(int));
+
+  try
+  {
+    ns::cuda::device_allocator<std::byte> alloc;
+
+    cuda::event before = initial_happening(ex);
+
+    cuda::event e = bulk_execute_with_workspace_after(ex, alloc, before, shape, workspace_shape, [=](ns::int2 coord, cuda::coop_executor::workspace_type ws)
+    {
+      // hash the coordinate and store the result in the array
+      int result = hash(coord);
+      array[coord.y][coord.x] = result;
+
+      auto [thread_id, block_id] = coord;
+
+      // test the outer workspace
+      {
+        std::span<int> block_indices = reinterpret_buffer<int>(get_buffer(ws));
+
+        // the leader of each block records its block index in the global workspace
+        if(thread_id == 0)
+        {
+          block_indices[block_id] = block_id;
+        }
+
+        // use the global barrier
+        arrive_and_wait(get_barrier(ws));
+
+        // the first thread of the first block checks that each block was recorded in the global workspace
+        if(thread_id == 0 and block_id == 0)
+        {
+          int expected = 0;
+          for(int id : block_indices)
+          {
+            assert(expected == id);
+            ++expected;
+          }
+        }
+      }
+
+      // test the local workspace
+      {
+        std::span<int> thread_indices = reinterpret_buffer<int>(get_buffer(get_local_workspace(ws)));
+
+        // each thread records its index in the local workspace
+        thread_indices[thread_id] = thread_id;
+
+        // use the local barrier
+        arrive_and_wait(get_barrier(get_local_workspace(ws)));
+
+        // the first thread of each block checks that each thread was recorded in the local workspace
+        if(thread_id == 0)
+        {
+          int expected = 0;
+          for(int id : thread_indices)
+          {
+            assert(expected == id);
+            ++expected;
+          }
+        }
+      }
+    });
+
+    wait(e);
+
+    for(auto coord : lattice(shape))
+    {
+      int expected = hash(coord);
+      int result = array[coord.y][coord.x];
+      assert(expected == result);
+    }
+  }
+  catch(std::runtime_error& e)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+template<ns::coordinate C>
+void test_execute_kernel_customization_point(ns::cuda::coop_executor ex, C shape)
+{
+  using namespace ns;
+
+  try
+  {
+    ns::execute_kernel(ex, shape, [=](C coord)
+    {
+      int i = apply_stride(compact_column_major_stride(shape), coord);
+      auto c = colexicographical_lift(i, array_shape);
+
+      array[c[0]][c[1]] = i;
+    });
+
+    for(int i = 0; i < ns::shape_size(array_shape); ++i)
+    {
+      auto c = colexicographical_lift(i, array_shape);
+
+      assert(i == array[c[0]][c[1]]);
+    }
+  }
+  catch(std::runtime_error& e)
+  {
+#if defined(__CUDACC__)
+    throw;
+#endif
+  }
+}
+
+
+void test(ns::cuda::coop_executor ex)
+{
+  test_equality(ex);
+  test_finally_execute_after(ex);
+  test_first_execute(ex);
+  test_execute_after(ex);
+
+  test_bulk_execute_after_member_function(ex);
+  test_bulk_execute_after_customization_point(ex);
+
+  test_bulk_execute_with_workspace_after_member_function(ex);
+  test_bulk_execute_with_workspace_after_customization_point(ex);
+
+  test_execute_kernel_customization_point(ex, array_shape[0]*array_shape[1]);
+  test_execute_kernel_customization_point(ex, ns::int2{array_shape[0], array_shape[1]});
+}
+
+
+void test_with_default_stream()
+{
+  cudaStream_t s{};
+
+  ns::cuda::coop_executor ex{0, s};
+  test(ex);
+}
+
+
+void test_with_new_stream()
+{
+  cudaStream_t s{};
+  assert(cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking) == cudaSuccess);
+
+  ns::cuda::coop_executor ex{0, s};
+  test(ex);
+
+  assert(cudaStreamDestroy(s) == cudaSuccess);
+}
+
+
+void test_coop_executor()
+{
+  test_concepts();
+  test_with_default_stream();
+  test_with_new_stream();
+}
+
---
+++ ubu/platform/cuda/coop_executor.hpp
@@ -0,0 +1,161 @@
+#pragma once
+
+#include <bit>
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <span>
+#include <ubu/ubu.hpp>
+#include <ubu/platform/cuda.hpp>
+#include <ubu/platform/cuda/detail/launch_as_cooperative_kernel.hpp>
+#include <ubu/platform/cuda/detail/sync_blocks.hpp>
+
+
+namespace ubu::cuda
+{
+namespace detail
+{
+
+
+template<std::invocable<ubu::int2> F>
+  requires std::is_trivially_copyable_v<F>
+struct invoke_with_int2
+{
+  F f;
+
+  constexpr void operator()() const
+  {
+#if defined(__CUDACC__)
+    if target(ubu::detail::is_device())
+    {
+      std::invoke(f, ubu::int2(threadIdx.x, blockIdx.x));
+    }
+#endif
+  }
+};
+
+
+} // end detail
+
+
+class coop_executor
+{
+  public:
+    using workspace_type = concurrent_device_workspace;
+    using workspace_shape_type = int2; // XXX ideally, this would simply be grabbed from workspace_type
+    using shape_type = workspace_shape_type;
+    using happening_type = cuda::event;
+
+    constexpr coop_executor(int device, cudaStream_t stream = cudaStream_t{}, std::size_t dynamic_smem_size = 0)
+      : device_{device},
+        stream_{stream},
+        dynamic_smem_size_{dynamic_smem_size}
+    {}
+
+    constexpr coop_executor()
+      : coop_executor(0)
+    {}
+
+    coop_executor(const coop_executor&) = default;
+
+    auto operator<=>(const coop_executor&) const = default;
+
+    template<congruent<shape_type> S, std::regular_invocable<S> F>
+      requires std::is_trivially_copyable_v<F>
+    event bulk_execute_after(const event& before, S shape, F f) const
+    {
+      // create the function that will be launched as a kernel
+      detail::invoke_with_int2<F> kernel{f};
+
+      // convert the shape to dim3s
+      auto [block_dim, grid_dim] = as_dim3s(shape);
+
+      // launch the kernel
+      return detail::launch_as_cooperative_kernel_after(before, grid_dim, block_dim, dynamic_smem_size_, stream_, device_, kernel);
+    }
+
+    template<std::regular_invocable F>
+      requires std::is_trivially_copyable_v<F>
+    event execute_after(const event& before, F f) const
+    {
+      return bulk_execute_after(before, ones<shape_type>, [f](shape_type)
+      {
+        // ignore the incoming parameter and just invoke the function
+        std::invoke(f);
+      });
+    }
+
+    template<congruent<shape_type> S, std::regular_invocable<S, workspace_type> F>
+      requires std::is_trivially_copyable_v<F>
+    event bulk_execute_with_workspace_after(const event& before, S shape, int2 workspace_shape, F f) const
+    {
+      // decompose workspace shape
+      auto [inner_buffer_size, outer_buffer_size] = workspace_shape;
+
+      // get an allocator for the outer workspace buffer
+      device_allocator alloc = get_allocator();
+
+      // allocate a zeroed outer buffer after the before event
+      auto [outer_buffer_ready, outer_buffer_ptr] = allocate_and_zero_after<std::byte>(alloc, *this, before, outer_buffer_size);
+
+      // launch the kernel after the outer buffer is ready
+      std::span<std::byte> outer_buffer(outer_buffer_ptr.to_raw_pointer(), outer_buffer_size);
+      event after_kernel = with_dynamic_smem_size(inner_buffer_size).bulk_execute_after(outer_buffer_ready, shape, [=](shape_type coord)
+      {
+        std::invoke(f, coord, workspace_type(outer_buffer));
+      });
+
+      // deallocate outer buffer after the kernel
+      return deallocate_after(alloc, std::move(after_kernel), outer_buffer_ptr, outer_buffer_size);
+    }
+
+    constexpr int device() const
+    {
+      return device_;
+    }
+
+    constexpr cudaStream_t stream() const
+    {
+      return stream_;
+    }
+
+    constexpr std::size_t dynamic_smem_size() const
+    {
+      return dynamic_smem_size_;
+    }
+
+    constexpr void dynamic_smem_size(std::size_t num_bytes)
+    {
+      dynamic_smem_size_ = num_bytes;
+    }
+
+  private:
+    constexpr coop_executor with_dynamic_smem_size(std::size_t dynamic_smem_size) const
+    {
+      return {device(), stream(), dynamic_smem_size};
+    }
+
+    // XXX this is only used to allocate a temporary buffer for bulk_execute_with_workspace_after
+    //     so, we should use a different type of allocator optimized for such use
+    device_allocator<std::byte> get_allocator() const
+    {
+      return {device_, stream_};
+    }
+
+    template<congruent<shape_type> S>
+    constexpr static std::pair<dim3,dim3> as_dim3s(S shape)
+    {
+      dim3 block_dim{static_cast<unsigned int>(get<0>(shape))};
+      dim3 grid_dim{static_cast<unsigned int>(get<1>(shape))};
+
+      return {block_dim, grid_dim};
+    }
+
+    int device_;
+    cudaStream_t stream_;
+    std::size_t dynamic_smem_size_;
+};
+
+
+} // end ubu::cuda
+
