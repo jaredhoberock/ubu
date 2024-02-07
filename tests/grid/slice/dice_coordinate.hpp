5ff68397a0f439086d411ac26695073d6b7c9506
Jared Hoberock <jaredhoberock@gmail.com>
2024-02-07 16:04:53 -0600

    Implement dice_coordinate with opposite_slicer and slice_coordinate

--- tests/grid/slice/dice_coordinate.hpp
+++ tests/grid/slice/dice_coordinate.hpp
@@ -1,4 +1,3 @@
-#include <cassert>
 #include <ubu/grid/coordinate/point.hpp>
 #include <ubu/grid/slice/dice_coordinate.hpp>
 
@@ -10,58 +9,65 @@ void test_dice_coordinate()
   using namespace ns;
 
   {
-    int  coord    = 1;
-    auto katana   = _;
-    tuple<>  expected;
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr int  coord    = 1;
+    constexpr auto katana   = _;
+    constexpr tuple<>  expected;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    ns::int2  coord(1,2);
-    tuple    katana(_,2);
-    int  expected = 2;
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr ns::int2  coord(1,2);
+    constexpr tuple    katana(_,2);
+    constexpr int  expected = 2;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    ns::int3   coord(1,2,3);
-    tuple     katana(1,_,_);
-    int   expected = 1;
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr ns::int3   coord(1,2,3);
+    constexpr tuple     katana(1,_,_);
+    constexpr int   expected = 1;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    ns::int1    coord(6);
-    auto     katana = _;
-    tuple<>    expected;
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr ns::int1    coord(6);
+    constexpr auto     katana = _;
+    constexpr tuple<>    expected;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    tuple    coord( ns::int3(1,2,3), ns::int1(6));
-    tuple    katana(ns::int3(1,2,3),          _);
-    auto expected = ns::int3(1,2,3);
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr tuple    coord( ns::int3(1,2,3), ns::int1(6));
+    constexpr tuple    katana(ns::int3(1,2,3),          _);
+    constexpr auto expected = ns::int3(1,2,3);
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    tuple  coord(    ns::int3(1,2,3),   ns::int2(4,5), ns::int1(6));
-    tuple katana(  make_tuple(1,2,3), make_tuple(_,5), _);
-    tuple expected(make_tuple(1,2,3), 5);
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr tuple  coord(    ns::int3(1,2,3),   ns::int2(4,5), ns::int1(6));
+    constexpr tuple katana(  make_tuple(1,2,3), make_tuple(_,5), _);
+    constexpr tuple expected(make_tuple(1,2,3), 5);
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    tuple    coord(tuple(tuple(1,2,3), tuple(4,5)));
-    tuple   katana(tuple(tuple(_,2,_), _));
-    int expected               = 2;
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr tuple    coord(tuple(tuple(1,2,3), tuple(4,5)));
+    constexpr tuple   katana(tuple(tuple(_,2,_), _));
+    constexpr int expected               = 2;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 
   {
-    tuple    coord(tuple(pair(1,2),3), tuple(4,5), tuple(6));
-    tuple   katana(tuple(pair(_,2),_), _,          tuple(_));
-    int expected              = 2;
-    assert(expected == ns::dice_coordinate(coord, katana));
+    constexpr tuple    coord(tuple(pair(1,2),3), tuple(4,5), tuple(6));
+    constexpr tuple   katana(tuple(pair(_,2),_), _,          tuple(_));
+    constexpr int expected              = 2;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
+  }
+
+  {
+    constexpr tuple    coord(1,_,_);
+    constexpr tuple   katana(1,_,_);
+    constexpr int expected = 1;
+    static_assert(expected == ns::dice_coordinate(coord, katana));
   }
 }
--- ubu/grid/slice/dice_coordinate.hpp
+++ ubu/grid/slice/dice_coordinate.hpp
@@ -2,85 +2,17 @@
 
 #include "../../detail/prologue.hpp"
 
-#include "../coordinate/concepts/coordinate.hpp"
-#include "../coordinate/detail/tuple_algorithm.hpp"
+#include "opposite_slicer.hpp"
+#include "slice_coordinate.hpp"
 #include "slicer.hpp"
-#include "underscore.hpp"
-#include <tuple>
-#include <utility>
 
 namespace ubu
 {
-namespace detail
+
+template<slicer C, slicer_for<C> K>
+constexpr slicer auto dice_coordinate(const C& coord, const K& katana)
 {
-
-
-// terminal case 1: the slicer is literally the underscore: discard the whole coordinate
-template<slicer C>
-constexpr std::tuple<> dice_coordinate_impl(const C& coord, detail::underscore_t)
-{
-  return {};
-}
-
-// terminal case 2: the slicer does not contain an underscore; keep the whole coordinate
-template<slicer C, slicer_for<C> S>
-  requires slicer_without_underscore<S>
-constexpr C dice_coordinate_impl(const C& coord, const S&)
-{
-  return coord;
-}
-
-// recursive case: the slicer is nonscalar
-// this is a forward declaration for recursive_dice_coordinate_impl
-template<slicer C, nonscalar_slicer_for<C> S>
-  requires slicer_with_underscore<S>
-constexpr auto dice_coordinate_impl(const C& coord, const S& katana);
-
-template<detail::tuple_like R, class Slicer, class Arg>
-constexpr auto wrap_if_no_underscore_or_int(const Arg& arg)
-{
-  if constexpr(slicer_without_underscore<Slicer> or std::integral<Arg>)
-  {
-    return make_tuple_similar_to<R>(arg);
-  }
-  else
-  {
-    return arg;
-  }
-}
-
-template<slicer C, nonscalar_slicer_for<C> S, std::size_t... I>
-constexpr auto recursive_dice_coordinate_impl(std::index_sequence<I...>, const C& coord, const S& katana)
-{
-  // we apply dice_coordinate to each element of coord & katana and we want to concatenate all the results
-  // we also want to preserve the tuple structure of any tuples that were selected by katana at element I
-  //
-  // so, if element I of katana has no underscore, we wrap that result in an extra tuple layer to preserve the tuple in the concatenation
-  // we also need to wrap dice_coordinate's result if it returns a raw integer for tuple_cat to work
-
-  // finally, because it's really inconvenient for this function to return a (single_thing), we unwrap any singles we find
-
-  auto result_tuple = tuple_cat_similar_to<C>(wrap_if_no_underscore_or_int<C,std::tuple_element_t<I,S>>(dice_coordinate_impl(get<I>(coord), get<I>(katana)))...);
-  return detail::tuple_unwrap_single(result_tuple);
-}
-
-
-// recursive case: the slicer is nonscalar
-template<slicer C, nonscalar_slicer_for<C> S>
-  requires slicer_with_underscore<S>
-constexpr auto dice_coordinate_impl(const C& coord, const S& katana)
-{
-  return recursive_dice_coordinate_impl(detail::tuple_indices<S>, coord, katana);
-}
-
-
-} // end detail
-
-
-template<slicer C, slicer_for<C> S>
-constexpr slicer auto dice_coordinate(const C& coord, const S& katana)
-{
-  return detail::dice_coordinate_impl(coord, katana);
+  return slice_coordinate(coord, opposite_slicer(coord, katana));
 }
 
 } // end ubu
