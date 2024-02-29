da19a4ba7b792fe31e3ed2fcc08eb80c5406fa04
Jared Hoberock <jaredhoberock@gmail.com>
2024-02-29 15:24:57 -0600

    Introduce layout_of_rank

--- ubu/tensor/layout/layout.hpp
+++ ubu/tensor/layout/layout.hpp
@@ -5,6 +5,7 @@
 #include "../coordinate/concepts/congruent.hpp"
 #include "../coordinate/concepts/coordinate.hpp"
 #include "../concepts/tensor_like.hpp"
+#include "../concepts/tensor_like_of_rank.hpp"
 #include "../traits/tensor_element.hpp"
 
 namespace ubu
@@ -21,6 +22,9 @@ concept layout_for =
   layout<L>
   and coordinate_for<tensor_element_t<L>, T>
 ;
+
+template<class L, std::size_t R>
+concept layout_of_rank = (layout<L> and tensor_rank_v<L> == R);
 
 } // end ubu
 
