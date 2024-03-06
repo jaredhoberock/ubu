1af8c22f098a92b8bffb8f47eb2fa5625a63e6a2
Jared Hoberock <jaredhoberock@gmail.com>
2024-03-06 13:00:46 -0600

    std::tuple_cat -> detail::tuple_cat_similar_to

--- ubu/tensor/detail/coordinate_cat.hpp
+++ ubu/tensor/detail/coordinate_cat.hpp
@@ -1,22 +1,21 @@
 #pragma once
 
 #include "../../detail/prologue.hpp"
-#include "../coordinate/concepts/coordinate.hpp"
+#include "../coordinate/concepts/semicoordinate.hpp"
 #include "../coordinate/detail/tuple_algorithm.hpp"
-#include "../slice/slicer.hpp"
 
 namespace ubu::detail
 {
 
 // XXX this function ought to be variadic
-template<slicer C1, coordinate C2>
-constexpr auto coordinate_cat(const C1& coord1, const C2& coord2)
+template<semicoordinate C1, semicoordinate C2>
+constexpr semicoordinate auto coordinate_cat(const C1& coord1, const C2& coord2)
 {
   // ensure that both coordinates are tuples before calling tuple_cat
   auto tuple1 = ensure_tuple(coord1);
   auto tuple2 = ensure_tuple(coord2);
 
-  return tuple_cat(tuple1,tuple2);
+  return tuple_cat_similar_to<decltype(tuple1)>(tuple1,tuple2);
 }
 
 } // end ubu::detail
