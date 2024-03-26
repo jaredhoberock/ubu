3046953a2cf96f1b3b8de8cb46d0fecf63fd4e16
Jared Hoberock <jaredhoberock@gmail.com>
2024-03-25 19:22:06 -0500

    Introduce and test block_like coop_exclusive_scan optional

--- ubu/platform/cuda/cooperation/block/coop_exclusive_scan.hpp
+++ ubu/platform/cuda/cooperation/block/coop_exclusive_scan.hpp
@@ -3,6 +3,7 @@
 #include "../../../../detail/prologue.hpp"
 
 #include "../../../../cooperation/uninitialized_coop_array.hpp"
+#include "../../../../cooperation/uninitialized_coop_optional_array.hpp"
 #include "../../../../cooperation/cooperator.hpp"
 #include "../warp/coop_inclusive_scan.hpp"
 #include "../warp/shuffle_up.hpp"
@@ -47,8 +48,75 @@ constexpr T coop_exclusive_scan(B group, T init, T value, F binary_op)
   }
 
   // accumulate the carry-in into our exclusive scan result,
-  // except each lane 0, whose result is simply the warp's carry-in
+  // except for each lane 0, whose result is simply its warp's carry-in
   return is_leader(subgroup(group)) ? carry_in : binary_op(carry_in, exclusive_value);
+}
+
+
+template<cuda::block_like B, class T, std::invocable<T,T> F>
+  requires (std::is_trivially_copy_constructible_v<T> and std::convertible_to<std::invoke_result_t<F,T,T>,T>)
+constexpr std::optional<T> coop_exclusive_scan(B group, std::optional<T> init, std::optional<T> value, F binary_op)
+{
+  // each warp does an inclusive scan
+  value = coop_inclusive_scan(subgroup(group), value, binary_op);
+
+  // shuffle to get the warp's exclusive scan (except for each lane 0)
+  std::optional exclusive_value = shuffle_up(subgroup(group), value, 1);
+
+  // each thread computes its warp's init by summing previous warp sums
+  uninitialized_coop_optional_array<T,B> warp_sums(group, subgroup_count(group));
+
+  if(is_last_in_group(subgroup(group)))
+  {
+    warp_sums[subgroup_id(group)] = value;
+  }
+
+  synchronize(group);
+
+  // each thread accumulates previous warps' sums
+  std::optional carry_in = init;
+
+  #pragma unroll
+  for(int warp = 1; warp < subgroup_count(group); ++warp)
+  {
+    if(subgroup_id(group) >= warp)
+    {
+      if(std::optional<T> other = warp_sums[warp-1]; other)
+      {
+        if(carry_in)
+        {
+          carry_in = binary_op(*carry_in, *other);
+        }
+        else
+        {
+          carry_in = other;
+        }
+      }
+    }
+  }
+
+  // accumulate the carry-in into our exclusive scan result,
+  // except for each lane 0, whose result is simply its warp's carry-in
+
+  if(is_leader(subgroup(group)))
+  {
+    exclusive_value = carry_in;
+  }
+  else if(carry_in)
+  {
+    if(exclusive_value)
+    {
+      exclusive_value = binary_op(*carry_in, *exclusive_value);
+    }
+    else
+    {
+      exclusive_value = carry_in;
+    }
+  }
+  
+  synchronize(group);
+
+  return exclusive_value;
 }
 
 
