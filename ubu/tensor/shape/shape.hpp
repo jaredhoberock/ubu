89f89f6230ba387aaa57208d66425144a53b7d68
Jared Hoberock <jaredhoberock@gmail.com>
2024-03-26 16:49:52 -0500

    Introduce shape_v and constant_shaped

--- ubu/tensor/shape/shape.hpp
+++ ubu/tensor/shape/shape.hpp
@@ -107,6 +107,29 @@ concept shaped =
 ;
 
 
+// note that the type of shape_v<T> is different from shape_t<T>
+// because, for example, this defintion "unwraps" a constant<13> to 13
+// XXX is this a good choice?
+template<shaped T>
+  requires constant_valued<shape_t<T>>
+constexpr inline auto shape_v = constant_value_v<shape_t<T>>;
+
+
+template<class T>
+concept constant_shaped =
+#if defined(__circle_lang__)
+  // WAR circle bug https://godbolt.org/z/erGajGP5P
+  shaped<T> and constant_valued<shape_t<T>>
+#else
+  requires()
+  {
+    // shape_v must exist
+    shape_v<T>;
+  }
+#endif
+;
+
+
 } // end ubu
 
 #include "../../detail/epilogue.hpp"
