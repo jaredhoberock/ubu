0fccb1f3b718399a8013ce63f945a7c5e7d51517
Jared Hoberock <jaredhoberock@gmail.com>
2024-04-03 17:34:53 -0500

    Introduce coordinate_element_v

--- ubu/tensor/coordinate/traits/coordinate_element.hpp
+++ ubu/tensor/coordinate/traits/coordinate_element.hpp
@@ -30,6 +30,13 @@ template<std::size_t I, semicoordinate T>
   requires (I < rank_v<T>)
 using coordinate_element_t = typename detail::coordinate_element<I,T>::type;
 
+// note that the type of coordinate_element_v<I,T> is different from coordinate_element_t<I,T>
+// because, for example, this definition "unwraps" a constant<13> to 13
+// XXX is this a good choice?
+template<std::size_t I, semicoordinate T>
+  requires (I < rank_v<T>) and constant_valued<coordinate_element_t<I,T>>
+constexpr inline auto coordinate_element_v = constant_value_v<coordinate_element_t<I,T>>;
+
 } // end ubu
 
 #include "../../../detail/epilogue.hpp"
