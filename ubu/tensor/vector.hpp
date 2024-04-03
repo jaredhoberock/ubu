9ee23f54a6dcfe30c70e9292e12b66b21dfb2bc3
Jared Hoberock <jaredhoberock@gmail.com>
2024-04-03 18:36:40 -0500

    Introduce span_like

--- ubu/tensor/vector.hpp
+++ ubu/tensor/vector.hpp
@@ -3,6 +3,7 @@
 #include "../detail/prologue.hpp"
 
 #include "vector/sized_vector_like.hpp"
+#include "vector/span_like.hpp"
 #include "vector/vector_like.hpp"
 
 #include "../detail/epilogue.hpp"
---
+++ ubu/tensor/vector/span_like.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../../detail/prologue.hpp"
+#include "../../memory/pointer/pointer_like.hpp"
+#include "../iterator.hpp"
+#include "sized_vector_like.hpp"
+#include <concepts>
+#include <span>
+
+namespace ubu
+{
+
+// something is span-like if it is 1D, sized, and provides access to its data
+template<class T>
+concept span_like =
+  sized_vector_like<T>
+  and requires(T vec)
+  {
+    // std::ranges::data isn't what we want because it
+    // requires .data() to return a raw pointer
+    { std::data(vec) } -> pointer_like;
+  }
+;
+
+} // end ubu
+
+#include "../../detail/epilogue.hpp"
+
