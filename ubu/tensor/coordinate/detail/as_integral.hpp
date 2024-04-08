090f167e0fd28ff47a1547442456379f9713ad1a
Jared Hoberock <jaredhoberock@gmail.com>
2024-04-08 18:55:16 -0500

    Generalize the implementation of as_integral

--- ubu/tensor/coordinate/detail/as_integral.hpp
+++ ubu/tensor/coordinate/detail/as_integral.hpp
@@ -2,21 +2,30 @@
 
 #include "../../../detail/prologue.hpp"
 #include "../../../miscellaneous/constant_valued.hpp"
-
 #include <concepts>
+#include <type_traits>
 
 namespace ubu::detail
 {
 
 template<class T>
-concept constant_valued_integral =
-  constant_valued<T>
-  and std::integral<constant_value_t<T>>
+concept convertible_to_integral_value_type =
+  requires()
+  {
+    // ::value_type must exist
+    typename std::remove_cvref_t<T>::value_type;
+  }
+
+  // ::value_type must be an integral type
+  and std::integral<typename std::remove_cvref_t<T>::value_type>
+
+  // T must be convertible to ::value_type
+  and std::convertible_to<T, typename std::remove_cvref_t<T>::value_type>
 ;
 
 // as_integral converts a type into an integral
 template<class I>
-  requires (std::integral<I> or constant_valued_integral<I>)
+  requires (std::integral<I> or convertible_to_integral_value_type<I>)
 constexpr std::integral auto as_integral(const I& i)
 {
   if constexpr (std::integral<I>)
@@ -26,8 +35,9 @@ constexpr std::integral auto as_integral(const I& i)
   }
   else
   {
-    // case 1: constant_value_v<I> is std::integral
-    return constant_value_v<I>;
+    // case 1: I is convertible to std::integral value_type
+    using value_type = typename std::remove_cvref_t<I>::value_type;
+    return static_cast<value_type>(i);
   }
 }
 
