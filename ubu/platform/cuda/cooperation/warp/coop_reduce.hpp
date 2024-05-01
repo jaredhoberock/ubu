c6080db376c31fa52e6021e41706b47b7e14ea34
Jared Hoberock <jaredhoberock@gmail.com>
2024-05-01 18:21:59 -0500

    Remove the special case of a full warp of values in warp_like coop_reduce Correct a bug in the general case of warp_like coop_reduce

--- ubu/platform/cuda/cooperation/warp/coop_reduce.hpp
+++ ubu/platform/cuda/cooperation/warp/coop_reduce.hpp
@@ -17,27 +17,20 @@ template<warp_like W, class T, std::invocable<T,T> F>
   requires (std::is_trivially_copy_constructible_v<T> and std::convertible_to<std::invoke_result_t<F,T,T>, T>)
 constexpr std::optional<T> coop_reduce(W self, std::optional<T> value, F binary_op)
 {
-  int num_values = synchronize_and_count(self, value.has_value());
-
-  auto num_threads = warp_size;
   auto num_passes = 5_c; // this is log2(warp_size)
 
-  if(num_values == num_threads)
+  for(int pass = 0; pass != num_passes; ++pass)
   {
-    for(int pass = 0; pass != num_passes; ++pass)
+    int offset = 1 << pass;
+    std::optional other = shuffle_down(self, value, offset);
+
+    if(value and other)
     {
-      int offset = 1 << pass;
-      T other = shuffle_down(self, *value, offset);
-      value = binary_op(*value, other);
+      value = binary_op(*value, *other);
     }
-  }
-  else
-  {
-    for(int pass = 0; pass != num_passes; ++pass)
+    else if(other)
     {
-      int offset = 1 << pass;
-      std::optional other = shuffle_down(self, value, offset);
-      if((id(self) + offset < num_values) and other) *value = binary_op(*value, *other);
+      value = other;
     }
   }
 
