6b4e8f74c825e257715394803b8aa67a5e0137da
Jared Hoberock <jaredhoberock@gmail.com>
2024-05-01 18:23:06 -0500

    Introduce warp_like overload of broadcast

--- ubu/platform/cuda/cooperation/warp.hpp
+++ ubu/platform/cuda/cooperation/warp.hpp
@@ -2,6 +2,7 @@
 
 #include "../../../detail/prologue.hpp"
 
+#include "warp/broadcast.hpp"
 #include "warp/coop_ballot.hpp"
 #include "warp/coop_exclusive_scan.hpp"
 #include "warp/coop_inclusive_scan.hpp"
