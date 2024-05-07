a8c8ede9ede56a0feb7a3e968ef831b3c2bf5c6b
Jared Hoberock <jaredhoberock@gmail.com>
2024-05-07 16:00:06 -0500

    Introduce asynchronous_allocation and asynchronous_allocation_congruent_with

--- ubu/memory/allocator/concepts.hpp
+++ ubu/memory/allocator/concepts.hpp
@@ -3,6 +3,7 @@
 #include "../../detail/prologue.hpp"
 
 #include "concepts/allocator.hpp"
+#include "concepts/asynchronous_allocation.hpp"
 #include "concepts/asynchronous_allocator.hpp"
 
 #include "../../detail/epilogue.hpp"
