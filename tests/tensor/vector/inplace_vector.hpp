79fabe65fdb8d35076a2ccb0235b6f8fa1d6caa3
Jared Hoberock <jaredhoberock@gmail.com>
2024-06-08 14:41:59 -0500

    Make inplace_vector of trivial T trivial itself This change got lost in the last merge

---
+++ tests/tensor/vector/inplace_vector.hpp
@@ -0,0 +1,12 @@
+#include <type_traits>
+#include <ubu/tensor/vector/inplace_vector.hpp>
+
+void test_inplace_vector()
+{
+  using namespace ubu;
+
+  static_assert(std::is_trivially_copy_constructible_v<inplace_vector<int,10>>);
+  static_assert(std::is_trivially_move_constructible_v<inplace_vector<int,10>>);
+  static_assert(std::is_trivially_destructible_v<inplace_vector<int,10>>);
+}
+
