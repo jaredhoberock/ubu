94653d89592fec2e0c34524e1bbbe3d2e9716550
Jared Hoberock <jaredhoberock@gmail.com>
2024-06-17 17:05:45 -0500

    Introduce composable concept Relax the constraints on compose and composed_view to allow A to be an invocable Test a simple application of ubu::transform to exercize the new relaxed constraints

--- ubu/tensors/views/compose.hpp
+++ ubu/tensors/views/compose.hpp
@@ -2,6 +2,7 @@
 
 #include "../../detail/prologue.hpp"
 
+#include "../concepts/composable.hpp"
 #include "../concepts/tensor_like.hpp"
 #include "composed_view.hpp"
 #include "layouts/layout.hpp"
@@ -16,15 +17,15 @@ namespace detail
 
 // composed_view and compose have a cyclic dependency and can't use each other directly
 // declare detail::make_composed_view for compose's use
-template<class A, layout_for<A> B>
-  requires std::is_object_v<A>
+template<class A, view B>
+  requires (std::is_object_v<A> and composable<A,B>)
 constexpr auto make_composed_view(A a, B b);
 
 template<class R, class A, class B>
 concept composition_of_tensors =
-  tensor_like<R>
-  and layout_for<B,A>
-  and std::same_as<shape_t<R>, shape_t<B>>
+  tensor_like<R> and
+  composable<A,B> and
+  std::same_as<shape_t<R>, shape_t<B>>
 ;
 
 template<class A, class B>
@@ -61,6 +62,7 @@ struct dispatch_compose
               and not has_compose_free_function<A&&,B&&>)
   constexpr tensor_like auto operator()(A&& a, B&& b) const
   {
+    // XXX this should call all on both arguments
     return detail::make_composed_view(std::forward<A>(a), std::forward<B>(b));
   }
 };
