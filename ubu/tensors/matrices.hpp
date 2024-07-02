0b68a4bb1e68405c115cc5c3ff893c1b39edf578
Jared Hoberock <jaredhoberock@gmail.com>
2024-07-02 18:47:23 -0500

    Introduce columns and rows

--- ubu/tensors/matrices.hpp
+++ ubu/tensors/matrices.hpp
@@ -4,11 +4,13 @@
 
 #include "matrices/column_major_layout.hpp"
 #include "matrices/column_major_like.hpp"
+#include "matrices/columns.hpp"
 #include "matrices/contiguous_column_major_matrix_like.hpp"
 #include "matrices/matrix_height.hpp"
 #include "matrices/matrix_like.hpp"
 #include "matrices/matrix_width.hpp"
 #include "matrices/row_major_layout.hpp"
+#include "matrices/rows.hpp"
 #include "matrices/transpose.hpp"
 
 #include "../detail/epilogue.hpp"
