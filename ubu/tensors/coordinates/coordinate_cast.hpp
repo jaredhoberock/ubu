fda03cafe621310f8de64f1993ee01e53d0fbf24
Jared Hoberock <jaredhoberock@gmail.com>
2024-07-30 15:34:14 -0500

    Relax coordinate_cast to allow T to be a semicoordinate

--- ubu/tensors/coordinates/coordinate_cast.hpp
+++ ubu/tensors/coordinates/coordinate_cast.hpp
@@ -4,8 +4,10 @@
 
 #include "../../utilities/tuples.hpp"
 #include "concepts/congruent.hpp"
+#include "concepts/coordinate.hpp"
 #include "concepts/semicoordinate.hpp"
 #include "detail/to_integral_like.hpp"
+#include "traits/rank.hpp"
 #include <concepts>
 #include <type_traits>
 
@@ -18,24 +20,44 @@ namespace ubu
 //     may not return exactly a T (in some cases, this is impossible).
 //     it promises only to return a result congruent to T,
 //     but it does its best to produce a T
-template<coordinate T, congruent<T> C>
+template<semicoordinate T, congruent<T> C>
   requires (not std::is_reference_v<T>)
 constexpr congruent<T> auto coordinate_cast(const C& coord)
 {
-  if constexpr (scalar_coordinate<T>)
+  if constexpr (ubu::rank_v<T> == 1)
   {
-    if constexpr (std::constructible_from<T,detail::to_integral_like_t<C>>)
+    // if T is directly constructible from C, just do that
+    if constexpr (std::constructible_from<T,const C&>)
     {
-      return T(detail::to_integral_like(coord));
+      return T(coord);
     }
+
+    // else, check if C is a full coordinate (not just a semicoordinate)
+    else if constexpr (scalar_coordinate<C>)
+    {
+      if constexpr (std::constructible_from<T,detail::to_integral_like_t<C>>)
+      {
+        // convert coord to an integral_like before constructing the T
+        return T(detail::to_integral_like(coord));
+      }
+      else
+      {
+        // no conversion is possible; return coord's integral_like representation
+        return detail::to_integral_like(coord);
+      }
+    }
+
+    // no conversion is possible; just return coord
     else
     {
-      // this case may happen if T is a ubu::constant, for example
-      return detail::to_integral_like(coord);
+      return coord;
     }
   }
   else
   {
+    // both T and C are tuple_like
+    static_assert(tuples::tuple_like<T> and tuples::tuple_like<C>);
+
     T zero{};
 
     return tuples::zip_with(zero, coord, [](auto z, const auto& c)
