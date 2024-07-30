3fac341f547a5e9f5c60923e0eae5cbaf7feda35
Jared Hoberock <jaredhoberock@gmail.com>
2024-07-30 14:17:02 -0500

    Relax the return type of coordinate_cast

--- ubu/tensors/coordinates/coordinate_cast.hpp
+++ ubu/tensors/coordinates/coordinate_cast.hpp
@@ -6,6 +6,7 @@
 #include "concepts/congruent.hpp"
 #include "concepts/coordinate.hpp"
 #include "detail/to_integral_like.hpp"
+#include <concepts>
 #include <type_traits>
 
 
@@ -13,23 +14,35 @@ namespace ubu
 {
 
 
-template<scalar_coordinate T, scalar_coordinate C>
+// XXX "cast" is maybe a bad name because this function
+//     may not return exactly a T (in some cases, this is impossible).
+//     it promises only to return a result congruent to T,
+//     but it does its best to produce a T
+template<coordinate T, congruent<T> C>
   requires (not std::is_reference_v<T>)
-constexpr T coordinate_cast(const C& coord)
+constexpr congruent<T> auto coordinate_cast(const C& coord)
 {
-  return static_cast<T>(detail::to_integral_like(coord));
-}
-
-template<nonscalar_coordinate T, congruent<T> C>
-  requires (not std::is_reference_v<T>)
-constexpr T coordinate_cast(const C& coord)
-{
-  T zero{};
-
-  return tuples::zip_with(zero, coord, [](auto z, const auto& c)
+  if constexpr (scalar_coordinate<T>)
   {
-    return coordinate_cast<decltype(z)>(c);
-  });
+    if constexpr (std::constructible_from<T,detail::to_integral_like_t<C>>)
+    {
+      return T(detail::to_integral_like(coord));
+    }
+    else
+    {
+      // this case may happen if T is a ubu::constant, for example
+      return detail::to_integral_like(coord);
+    }
+  }
+  else
+  {
+    T zero{};
+
+    return tuples::zip_with(zero, coord, [](auto z, const auto& c)
+    {
+      return coordinate_cast<decltype(z)>(c);
+    });
+  }
 }
 
 
