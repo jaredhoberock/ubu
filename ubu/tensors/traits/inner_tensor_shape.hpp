8add9feff5f39631217f932b59139aed0c1dbec1
Jared Hoberock <jaredhoberock@gmail.com>
2024-07-31 14:00:26 -0500

    Fix typos in inner_tensor_shape_t

--- ubu/tensors/traits/inner_tensor_shape.hpp
+++ ubu/tensors/traits/inner_tensor_shape.hpp
@@ -2,13 +2,14 @@
 
 #include "../../detail/prologue.hpp"
 #include "../concepts/nested_tensor_like.hpp"
-#include "tensor_element.hpp"
+#include "inner_tensor.hpp"
+#include "tensor_shape.hpp"
 
 namespace ubu
 {
 
 template<nested_tensor_like T>
-using inner_tensor_t = tensor_element_t<T>;
+using inner_tensor_shape_t = tensor_shape_t<inner_tensor_t<T>>;
 
 } // end ubu
 
