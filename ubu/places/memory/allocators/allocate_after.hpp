96f5988465fabe35515d911cabbdd2302205f302
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-01 15:33:48 -0500

    Reorganize allocate_after CPO into detail::custom_allocate_after + default rebind allocator path

--- ubu/places/memory/allocators/allocate_after.hpp
+++ ubu/places/memory/allocators/allocate_after.hpp
@@ -2,6 +2,7 @@
 
 #include "../../../detail/prologue.hpp"
 
+#include "detail/custom_allocate_after.hpp"
 #include "rebind_allocator.hpp"
 #include <memory>
 #include <type_traits>
@@ -14,96 +15,31 @@ namespace detail
 {
 
 
-template<class T, class A, class E, class N>
-concept has_allocate_after_member_function_template = requires(A alloc, E before, N n)
-{
-  // XXX this should check that the result is a pair<E,pointer>
-  alloc.template allocate_after<T>(before, n);
-};
-
-
-template<class T, class A, class E , class N>
-concept has_allocate_after_free_function_template = requires(A alloc, E before, N n)
-{
-  // XXX this should check that the result is a pair<E,pointer>
-  allocate_after<T>(alloc, before, n);
-};
-
-
-template<class T, class A, class E, class N>
-concept has_allocate_after_member_function = requires(A alloc, E before, N n)
-{
-  requires std::same_as<T, typename std::allocator_traits<std::remove_cvref_t<A>>::value_type>;
-
-  // XXX this should check that the result is a pair<E,pointer>
-  alloc.allocate_after(before, n);
-};
-
-
-template<class T, class A, class E, class N>
-concept has_allocate_after_free_function = requires(A alloc, E before, N n)
-{
-  requires std::same_as<T, typename std::allocator_traits<std::remove_cvref_t<A>>::value_type>;
-
-  // XXX this should check that the result is a pair<E,pointer>
-  allocate_after(alloc, before, n);
-};
-
-
 // this is the type of allocate_after
 template<class T>
 struct dispatch_allocate_after
 {
-  // this dispatch path calls a member function template
-  template<class Alloc, class Event, class N>
-    requires has_allocate_after_member_function_template<T, Alloc&&, Event&&, N&&>
-  constexpr auto operator()(Alloc&& alloc, Event&& before, N&& n) const
+  // this dispatch path calls the allocator's customization of allocate_after
+  template<class A, class B, class S>
+    requires has_custom_allocate_after<T, A&&, B&&, S&&>
+  constexpr auto operator()(A&& alloc, B&& before, S&& shape) const
   {
-    return std::forward<Alloc>(alloc).template allocate_after<T>(std::forward<Event>(before), std::forward<N>(n));
+    return custom_allocate_after<T>(std::forward<A>(alloc), std::forward<B>(before), std::forward<S>(shape));
   }
 
-  // this dispatch path calls a free function template
-  template<class Alloc, class Event, class N>
-    requires (!has_allocate_after_member_function_template<T, Alloc&&, Event&&, N&&>
-              and has_allocate_after_free_function_template<T, Alloc&&, Event&&, N&&>)
-  constexpr auto operator()(Alloc&& alloc, Event&& before, N&& n) const
+  // this dispatch path rebinds the allocator and recurses
+  template<class A, class B, class S>
+    requires (not has_custom_allocate_after<T, A&&, B&&, S&&>
+              and has_rebind_allocator<T, A&&>
+              and has_custom_allocate_after<
+                T, rebind_allocator_result_t<T,A&&>, B&&, S&&
+              >)
+  constexpr auto operator()(A&& alloc, B&& before, S&& shape) const
   {
-    return allocate_after<T>(std::forward<Alloc>(alloc), std::forward<Event>(before), std::forward<N>(n));
-  }
+    auto rebound_alloc = rebind_allocator<T>(std::forward<A>(alloc));
 
-  // this dispatch path calls the member function
-  template<class Alloc, class Event, class N>
-    requires (!has_allocate_after_member_function_template<T, Alloc&&, Event&&, N&&>
-              and !has_allocate_after_free_function_template<T, Alloc&&, Event&&, N&&>
-              and has_allocate_after_member_function<T, Alloc&&, Event&&, N&&>)
-  constexpr auto operator()(Alloc&& alloc, Event&& before, N&& n) const
-  {
-    return std::forward<Alloc>(alloc).allocate_after(std::forward<Event>(before), std::forward<N>(n));
-  }
-
-  // this dispatch path calls the free function
-  template<class Alloc, class Event, class N>
-    requires (!has_allocate_after_member_function_template<T, Alloc&&, Event&&, N&&>
-              and !has_allocate_after_free_function_template<T, Alloc&&, Event&&, N&&>
-              and !has_allocate_after_member_function<T, Alloc&&, Event&&, N&&>
-              and has_allocate_after_free_function<T, Alloc&&, Event&&, N&&>)
-  constexpr auto operator()(Alloc&& alloc, Event&& before, N&& n) const
-  {
-    return allocate_after(std::forward<Alloc>(alloc), std::forward<Event>(before), std::forward<N>(n));
-  }
-
-  // this dispatch path calls the free function
-  template<class Alloc, class Event, class N>
-    requires (!has_allocate_after_member_function_template<T, Alloc&&, Event&&, N&&>
-              and !has_allocate_after_free_function_template<T, Alloc&&, Event&&, N&&>
-              and !has_allocate_after_member_function<T, Alloc&&, Event&&, N&&>
-              and !has_allocate_after_free_function<T, Alloc&&, Event&&, N&&>
-              and has_rebind_allocator<T, Alloc&&>)
-  constexpr decltype(auto) operator()(Alloc&& alloc, Event&& before, N&& n) const
-  {
-    auto rebound_alloc = rebind_allocator<T>(std::forward<Alloc>(alloc));
-
-    return (*this)(rebound_alloc, std::forward<Event>(before), std::forward<N>(n));
+    // XXX there's not a real need to recurse, we should simply call custom_allocate_after directly
+    return (*this)(rebound_alloc, std::forward<B>(before), std::forward<S>(shape));
   }
 };
 
@@ -120,8 +56,8 @@ constexpr detail::dispatch_allocate_after<T> allocate_after;
 } // end anonymous namespace
 
 
-template<class T, class A, class E, class N>
-using allocate_after_result_t = decltype(ubu::allocate_after<T>(std::declval<A>(), std::declval<E>(), std::declval<N>()));
+template<class T, class A, class E, class S>
+using allocate_after_result_t = decltype(ubu::allocate_after<T>(std::declval<A>(), std::declval<E>(), std::declval<S>()));
 
 
 } // end ubu
