3d6a00ffba4400e93b212d572f069866c4f0dda2
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-01 17:54:07 -0500

    Introduce coordinate_equal

--- ubu/tensors/coordinates/comparisons.hpp
+++ ubu/tensors/coordinates/comparisons.hpp
@@ -3,6 +3,7 @@
 #include "../../detail/prologue.hpp"
 
 #include "comparisons/colexicographical_compare_coordinates.hpp"
+#include "comparisons/coordinate_equal.hpp"
 #include "comparisons/is_below.hpp"
 #include "comparisons/lexicographical_compare_coordinates.hpp"
 
---
+++ ubu/tensors/coordinates/comparisons/coordinate_equal.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../../../detail/prologue.hpp"
+
+#include "../../../utilities/tuples.hpp"
+#include "../concepts/congruent.hpp"
+#include "../concepts/coordinate.hpp"
+#include "../detail/to_integral_like.hpp"
+
+namespace ubu
+{
+
+template<ubu::coordinate A, ubu::congruent<A> B>
+constexpr bool coordinate_equal(const A& a, const B& b)
+{
+  if constexpr (rank_v<A> == 1)
+  {
+    return detail::to_integral_like(a) == detail::to_integral_like(b);
+  }
+  else
+  {
+    return tuples::inner_product(a, b, [](auto a_i, auto b_i)
+    {
+      return coordinate_equal(a_i, b_i);
+    },
+    [](bool a, bool b)
+    {
+      return a and b;
+    });
+  }
+}
+
+} // end ubu
+
+#include "../../../detail/epilogue.hpp"
+
