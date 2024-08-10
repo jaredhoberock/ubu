917cb4b3e85fd54b34db9296db395aa8e7af167c
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-09 20:09:39 -0500

    Introduce cooperator_pointer trait

---
+++ ubu/cooperators/traits/cooperator_pointer.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../../detail/prologue.hpp"
+#include "../concepts/allocating_cooperator.hpp"
+#include "../primitives/coop_alloca.hpp"
+#include <utility>
+
+namespace ubu
+{
+
+template<allocating_cooperator C>
+using cooperator_pointer_t = decltype(coop_alloca(std::declval<C>()));
+
+} // end ubu
+
+#include "../../detail/epilogue.hpp"
+
