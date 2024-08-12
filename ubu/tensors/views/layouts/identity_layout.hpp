9e6d41d6a5113f8f2e60f479b45d518f278755f9
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-12 18:06:00 -0500

    Mark identity_layout::operator[] const Introduce identity_layout::size

--- ubu/tensors/views/layouts/identity_layout.hpp
+++ ubu/tensors/views/layouts/identity_layout.hpp
@@ -2,8 +2,11 @@
 
 #include "../../../detail/prologue.hpp"
 
+#include "../../coordinates/concepts/congruent.hpp"
+#include "../../coordinates/concepts/weakly_congruent.hpp"
 #include "../../coordinates/concepts/coordinate.hpp"
 #include "../../coordinates/colexicographical_lift.hpp"
+#include "../../shapes/shape_size.hpp"
 #include "../view_base.hpp"
 
 namespace ubu
@@ -20,7 +23,7 @@ class identity_layout : public view_base
     identity_layout(const identity_layout&) = default;
 
     template<weakly_congruent<S> C>
-    constexpr S operator[](const C& coord)
+    constexpr congruent<S> auto operator[](const C& coord) const
     {
       return colexicographical_lift(coord, shape());
     }
@@ -28,6 +31,11 @@ class identity_layout : public view_base
     constexpr S shape() const
     {
       return shape_;
+    }
+
+    constexpr std::size_t size() const
+    {
+      return shape_size(shape());
     }
 
     constexpr S coshape() const
