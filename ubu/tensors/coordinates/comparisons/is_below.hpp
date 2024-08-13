45c8daa93b2149bf736ed03b0ede346d76f680ad
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-13 18:23:20 -0500

    Eliminate use of scalar_coordinate & nonscalar_coordinate in is_below and is_below_or_equal

--- ubu/tensors/coordinates/comparisons/is_below.hpp
+++ ubu/tensors/coordinates/comparisons/is_below.hpp
@@ -6,6 +6,7 @@
 #include "../concepts/congruent.hpp"
 #include "../concepts/coordinate.hpp"
 #include "../detail/to_integral_like.hpp"
+#include "../traits/rank.hpp"
 
 
 namespace ubu
@@ -14,49 +15,54 @@ namespace ubu
 
 // is_below(lhs, rhs) returns true if all modes of lhs is < their corresponding mode of rhs
 
-template<scalar_coordinate C1, scalar_coordinate C2>
-constexpr bool is_below(const C1& lhs, const C2& rhs)
+template<coordinate A, coordinate B>
+  requires congruent<A,B>
+constexpr bool is_below(const A& a, const B& b)
 {
-  return detail::to_integral_like(lhs) < detail::to_integral_like(rhs);
-}
-
-template<nonscalar_coordinate C1, nonscalar_coordinate C2>
-  requires congruent<C1,C2>
-constexpr bool is_below(const C1& lhs, const C2& rhs)
-{
-  auto tuple_of_results = tuples::zip_with(lhs, rhs, [](auto l, auto r)
+  if constexpr (rank_v<A> == 0)
   {
-    return is_below(l, r);
-  });
-
-  return tuples::all_of(tuple_of_results, [](bool result)
+    return false;
+  }
+  else if constexpr (rank_v<A> == 1)
   {
-    return result;
-  });
+    return detail::to_integral_like(a) < detail::to_integral_like(b);
+  }
+  else
+  {
+    auto tuple_of_results = tuples::zip_with(a, b, [](auto a_i, auto b_i)
+    {
+      return is_below(a_i, b_i);
+    });
+
+    return tuples::all_of(tuple_of_results, [](bool result)
+    {
+      return result;
+    });
+  }
 }
 
 
 // is_below_or_equal(lhs, rhs) returns true if all modes of lhs is <= their corresponding mode of rhs
 
-template<scalar_coordinate C1, scalar_coordinate C2>
-constexpr bool is_below_or_equal(const C1& lhs, const C2& rhs)
+template<coordinate A, coordinate B>
+constexpr bool is_below_or_equal(const A& a, const B& b)
 {
-  return detail::to_integral_like(lhs) <= detail::to_integral_like(rhs);
-}
-
-template<nonscalar_coordinate C1, nonscalar_coordinate C2>
-  requires congruent<C1,C2>
-constexpr bool is_below_or_equal(const C1& lhs, const C2& rhs)
-{
-  auto tuple_of_results = tuples::zip_with(lhs, rhs, [](auto l, auto r)
+  if constexpr (rank_v<A> == 1)
   {
-    return is_below_or_equal(l, r);
-  });
-
-  return tuples::all_of(tuple_of_results, [](bool result)
+    return detail::to_integral_like(a) <= detail::to_integral_like(b);
+  }
+  else
   {
-    return result;
-  });
+    auto tuple_of_results = tuples::zip_with(a, b, [](auto a_i, auto b_i)
+    {
+      return is_below_or_equal(a_i, b_i);
+    });
+
+    return tuples::all_of(tuple_of_results, [](bool result)
+    {
+      return result;
+    });
+  }
 }
 
 
