8545c1c930cf957bd0adaf92c754597cd6aa0a34
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-21 22:04:46 -0500

    Handle 0-rank coordinates in in_domain

--- ubu/tensors/shapes/in_domain.hpp
+++ ubu/tensors/shapes/in_domain.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-#include "../coordinates/concepts/congruent.hpp"
+#include "../../detail/prologue.hpp"
+
+#include "../../utilities/integrals/size.hpp"
+#include "../coordinates/comparisons/is_below.hpp"
 #include "../coordinates/concepts/coordinate.hpp"
+#include "../coordinates/traits/rank.hpp"
 #include "shape.hpp"
 
 namespace ubu
@@ -10,7 +14,12 @@ namespace ubu
 template<shaped T, coordinate_for<T> C>
 constexpr bool in_domain(const T& tensor, const C& coord)
 {
-  if constexpr (sized<T> and integral_like<C>)
+  if constexpr (rank_v<C> == 0)
+  {
+    // a rank-0 coord for a rank-0 tensor is defined to be in its domain
+    return true;
+  }
+  else if constexpr (sized<T> and integral_like<C>)
   {
     // treat sized, 1D tensors as a special case
     // in such a case, size may be smaller than shape
@@ -24,4 +33,6 @@ constexpr bool in_domain(const T& tensor, const C& coord)
 }
 
 } // end ubu
+
+#include "../../detail/epilogue.hpp"
 
