9b7a771f3125e90fa8e058e4bef262fc4ad435c5
Jared Hoberock <jaredhoberock@gmail.com>
2024-08-26 15:17:14 -0500

    Eliminate std::allocator_traits case entirely from maybe_allocator_shape

--- ubu/places/memory/allocators/traits/detail/maybe_allocator_shape.hpp
+++ ubu/places/memory/allocators/traits/detail/maybe_allocator_shape.hpp
@@ -2,7 +2,6 @@
 
 #include "../../../../../detail/prologue.hpp"
 
-#include <memory>
 #include <type_traits>
 
 namespace ubu::detail
@@ -30,21 +29,6 @@ template<class A>
 struct maybe_allocator_shape<A>
 {
   using type = typename std::remove_cvref_t<A>::shape_type;
-};
-
-template<class A>
-concept has_allocator_traits_size_type = requires
-{
-  typename std::allocator_traits<std::remove_cvref_t<A>>::size_type;
-};
-
-// if std::allocator_traits<A>::size_type exists, use it
-template<class A>
-  requires (not has_member_shape_type<A>
-            and has_allocator_traits_size_type<A>)
-struct maybe_allocator_shape<A>
-{
-  using type = typename std::allocator_traits<std::remove_cvref_t<A>>::size_type;
 };
 
 
